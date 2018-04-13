1621e22f857b7758da5b1dc7e462526e9a4530d5
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-04-13 15:33:50 +0200

    CHANNELS > EMAIL SECTION: added the project name to the email address

--- src/app/channels/channels.component.ts
+++ src/app/channels/channels.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../core/auth.service';
+import { Project } from '../models/project-model';
+
 @Component({
   selector: 'app-channels',
   templateUrl: './channels.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChannelsComponent implements OnInit {
 
-  constructor() { }
+  project: Project;
+  project_name: string;
+
+  constructor(
+    public auth: AuthService,
+  ) { }
 
   ngOnInit() {
+    this.getCurrentProject()
   }
+  getCurrentProject() {
+    this.auth.project_bs.subscribe((project) => {
+      this.project = project
+      console.log('00 -> CHANNELS project from AUTH service subscription  ', this.project)
 
+      if(this.project){
+        this.project_name = this.project.name;
+      }
+
+    });
+  }
 }
