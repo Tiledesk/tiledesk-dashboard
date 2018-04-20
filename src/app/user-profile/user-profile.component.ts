251770570a132cd35278c4956e942e3fa44d86b1
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-04-20 16:05:31 +0200

    hided the sidebar in the user profile if the user views his profile but has not yet selected a project

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../core/auth.service';
 
 // USED FOR go back last page
 import { Location } from '@angular/common';
+import { Project } from '../models/project-model';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,13 +12,41 @@ import { Location } from '@angular/common';
 })
 export class UserProfileComponent implements OnInit {
   user: any;
+  project: Project;
+
   constructor(
     public auth: AuthService,
     private _location: Location
   ) { }
 
   ngOnInit() {
-    this.getLoggedUser()
+    this.getLoggedUser();
+
+    
+
+    this.getCurrentProject();
+  }
+
+
+  getCurrentProject() {
+    this.auth.project_bs.subscribe((project) => {
+      this.project = project
+
+
+      if (this.project) {
+        console.log('00 -> USER PROFILE project from AUTH service subscription  ', project)
+      } else {
+        console.log('00 -> USER PROFILE project from AUTH service subscription ? ', project)
+        this.selectSidebar();
+      }
+    });
+  }
+
+  // hides the sidebar if the user views his profile but has not yet selected a project
+  selectSidebar() {
+    const elemAppSidebar = <HTMLElement>document.querySelector('app-sidebar');
+    console.log('USER PROFILE  elemAppSidebar ', elemAppSidebar)
+    elemAppSidebar.setAttribute('style', 'display:none;');
   }
 
   getLoggedUser() {
