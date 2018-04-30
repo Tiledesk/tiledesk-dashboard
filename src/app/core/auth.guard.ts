8a9b47981f81492f0c477d8dead23cb8ea73ae85
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-04-30 12:50:33 +0200

    the AUTH GUARD service has been modified to not block access to the email verification page if the user checks the email but is not logged in the dashboard

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -11,17 +11,21 @@ import { map, take, tap } from 'rxjs/operators';
 
 import * as firebase from 'firebase/app';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   // public IS_LOGGED_IN: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
+  route: string;
   user: any;
+  is_verify_email_page: boolean;
+
   constructor(
     private auth: AuthService,
     private router: Router,
-    private notify: NotifyService
+    private notify: NotifyService,
+    public location: Location
   ) {
     console.log('HELLO AUTH GUARD !!!')
 
@@ -33,7 +37,27 @@ export class AuthGuard implements CanActivate {
       console.log('AUTH GUARD USER ', user)
     });
 
-    this.canActivate()
+    this.detectVerifyEmailRoute();
+
+    this.canActivate();
+
+  }
+
+  detectVerifyEmailRoute() {
+
+    // this.router.events.subscribe((val) => {
+      if (this.location.path() !== '') {
+        this.route = this.location.path();
+        console.log('AUTH GUARD »> »> ', this.route);
+        if (this.route.indexOf('/verify') !== -1) {
+          // this.router.navigate([`${this.route}`]);
+          this.is_verify_email_page = true;
+        } else {
+          this.is_verify_email_page = false;
+        }
+      }
+
+    // });
   }
 
   // canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
@@ -67,15 +91,20 @@ export class AuthGuard implements CanActivate {
   canActivate() {
     console.log('AlwaysAuthGuard');
 
-    if (this.user) {
+    if ((this.user) || (this.is_verify_email_page === true))  {
       // this.router.navigate(['/home']);
       return true;
+      // if ((!this.user))
     } else {
       // tslint:disable-next-line:no-debugger
       // debugger
       this.router.navigate(['/login']);
       return false;
     }
+    // else if ((this.is_verify_email_page === false)) {
+    //   this.router.navigate(['verify/email/']);
+    //   return false;
+    // }
   }
 
 
