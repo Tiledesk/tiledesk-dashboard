43adccd5355deb5653b72fd31733e38911d1f0ae
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-05-02 14:25:30 +0200

    page REQUEST-MSGS: in progress

--- src/app/requests-msgs/requests-msgs.component.ts
+++ src/app/requests-msgs/requests-msgs.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { Message } from '../models/message-model';
+import { RequestsService } from '../services/requests.service';
 
 @Component({
   selector: 'app-requests-msgs',
@@ -10,10 +12,12 @@ import { ActivatedRoute } from '@angular/router';
 export class RequestsMsgsComponent implements OnInit {
 
   id_request: string;
+  messagesList: Message[];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private requestsService: RequestsService
   ) { }
 
   ngOnInit() {
@@ -24,7 +28,38 @@ export class RequestsMsgsComponent implements OnInit {
   getRequestId() {
     this.id_request = this.route.snapshot.params['requestid'];
     console.log('REQUESTS-LIST COMP HAS PASSED REQUEST-ID ', this.id_request);
+
+    if (this.id_request) {
+      this.getMessagesList();
+    }
   }
+
+  /**
+   * REQUEST' MESSAGES (on FIRESTORE the COLLECTION is 'MESSAGES')
+   */
+  getMessagesList() {
+    // SUBSCIPTION TO snapshotChanges
+    this.requestsService.getSnapshotMsg(this.id_request)
+      .subscribe((data) => {
+        this.messagesList = data;
+        console.log('REQUESTS-MSGS.COMP: SUBSCRIPTION TO getSnapshot MSG ', data);
+        // this.showSpinner = false;
+        // console.log('TIMESTAMP ', this.messagesList);
+        // if (data.length) {
+        // this.scrollToBottom();
+        // }
+      },
+        (err) => {
+          console.log('GET MESSAGES LIST ERROR ', err);
+        },
+        () => {
+          console.log('GET MESSAGES LIST * COMPLETE *');
+          // this.showSpinner = false;
+        });
+
+  }
+
+
 
   cut_support_group_from_request_id(request_id: string) {
     if (request_id) {
