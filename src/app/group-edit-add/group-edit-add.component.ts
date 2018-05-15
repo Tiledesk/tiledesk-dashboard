9585fcdac2be76a2b4a7ab675a8583fda6369aaf
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-05-15 14:37:14 +0200

    built the TEMPLATE of  the component GROUP-EDIT-ADD

--- src/app/group-edit-add/group-edit-add.component.ts
+++ src/app/group-edit-add/group-edit-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-group-edit-add',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-edit-add.component.scss']
 })
 export class GroupEditAddComponent implements OnInit {
+  CREATE_VIEW = false;
+  EDIT_VIEW = false;
+  showSpinner = true;
+  groupName: string;
+  project_id: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+    this.detectsCreateEditInTheUrl();
+
+    this.getCurrentProject();
   }
+
+
+  detectsCreateEditInTheUrl() {
+    if (this.router.url.indexOf('/create') !== -1) {
+      console.log('HAS CLICKED CREATE ');
+
+      this.CREATE_VIEW = true;
+
+      this.showSpinner = false;
+    } else {
+      console.log('HAS CLICKED EDIT ');
+      this.EDIT_VIEW = true;
+    }
+  }
+
+  getCurrentProject() {
+    this.auth.project_bs.subscribe((project) => {
+
+      if (project) {
+        this.project_id = project._id
+        console.log('00 -> GROUP-EDIT-ADD-COMP project ID from AUTH service subscription ', this.project_id )
+      }
+
+    });
+  }
+
+  goBackGroupList() {
+    this.router.navigate(['project/' + this.project_id + '/groups']);
+  }
+
+
 
 }
