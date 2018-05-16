b908038852dc4edf4c33329b15e40f113c0531fa
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-05-16 19:21:55 +0200

    GROUP PAGE: added modal window for the selection of the members of a group

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../core/auth.service';
 import { GroupService } from '../services/group.service';
 import { Group } from '../models/group-model';
 import { Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-groups',
@@ -14,11 +15,15 @@ export class GroupsComponent implements OnInit {
   showSpinner = true;
   groupsList: Group[];
   project_id: string;
+  display_users_list_modal = 'none';
+  group_name: string;
+  projectUsersList: any;
 
   constructor(
     private auth: AuthService,
     private groupsService: GroupService,
     private router: Router,
+    private usersService: UsersService
   ) { }
 
   ngOnInit() {
@@ -65,4 +70,39 @@ export class GroupsComponent implements OnInit {
     this.router.navigate(['project/' + this.project_id + '/group/create']);
   }
 
+  open_user_list_modal(group_name: string) {
+    this.group_name = group_name;
+    this.getAllUsersOfCurrentProject();
+    this.display_users_list_modal = 'block';
+  }
+
+  getAllUsersOfCurrentProject() {
+    this.usersService.getProjectUsersByProjectId().subscribe((projectUsers: any) => {
+      console.log('GROUPS-COMP PROJECT-USERS (FILTERED FOR PROJECT ID)', projectUsers);
+
+      // this.showSpinner = false;
+      this.projectUsersList = projectUsers;
+
+    },
+      error => {
+        // this.showSpinner = false;
+        console.log('PROJECT USERS (FILTERED FOR PROJECT ID) - ERROR', error);
+      },
+      () => {
+        console.log('PROJECT USERS (FILTERED FOR PROJECT ID) - COMPLETE');
+      });
+  }
+
+  onCloseModal() {
+    this.display_users_list_modal = 'none';
+  }
+
+  onCloseModalHandled() {
+    this.display_users_list_modal = 'none';
+  }
+
+  selectUser(user_id) {
+    console.log('USER ID SELECTED ', user_id)
+
+  }
 }
