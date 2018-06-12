41175c3380440a2f5ca459944d78bd7d7b1827a6
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-06-12 19:50:38 +0200

    CHANGE PSW BTN is disabled if confirm password is different of new password

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -12,7 +12,7 @@ export class ChangePasswordComponent implements OnInit {
   oldPassword: string;
   newPassword: string;
   confirmNewPassword: string;
-
+  DISABLE_UPDATE_PSW_BTN = true
 
   constructor(
     private _location: Location
@@ -25,5 +25,25 @@ export class ChangePasswordComponent implements OnInit {
   goBack() {
     this._location.back();
   }
+
+
+  onDigitNewPsw() {
+
+    console.log('==> new psw ', this.newPassword);
+  }
+
+  onConfirmNewPsw() {
+    console.log('==> confirm new psw ', this.confirmNewPassword);
+
+    if ( this.confirmNewPassword !== this.newPassword) {
+      this.DISABLE_UPDATE_PSW_BTN = true;
+      console.log('CONFIRM PASSWORD IS NOT EQUAL TO NEW PASSWORD ' , this.DISABLE_UPDATE_PSW_BTN)
+    } else {
+      this.DISABLE_UPDATE_PSW_BTN = false;
+      console.log('CONFIRM PASSWORD IS NOT EQUAL TO NEW PASSWORD ' , this.DISABLE_UPDATE_PSW_BTN)
+    }
+
+  }
+
 
 }
