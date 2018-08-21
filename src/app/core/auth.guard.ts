36d29c02f5084d9a6d2e3a22f73ee198c9b68131
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-08-21 14:22:48 +0200

    IN PROGRESS: initialize the new project when the id of the project get from url does not match with the current project id

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 // ActivatedRouteSnapshot, RouterStateSnapshot,
-import { CanActivate, Router } from '@angular/router';
+// tslint:disable-next-line:max-line-length
+import { CanActivate, Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized, ActivatedRoute } from '@angular/router';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthService } from './auth.service';
@@ -12,6 +13,9 @@ import { map, take, tap } from 'rxjs/operators';
 import * as firebase from 'firebase/app';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Location } from '@angular/common';
+import { ProjectService } from '../services/project.service';
+
+import 'rxjs/add/operator/pairwise';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -23,11 +27,16 @@ export class AuthGuard implements CanActivate {
   is_signup_page: boolean;
   is_reset_psw_page: boolean;
 
+  nav_project_id: string;
+  current_project_id: string;
+
   constructor(
     private auth: AuthService,
     private router: Router,
     private notify: NotifyService,
-    public location: Location
+    public location: Location,
+    private activatedRoute: ActivatedRoute,
+    private projectService: ProjectService
   ) {
     console.log('HELLO AUTH GUARD !!!')
 
@@ -44,10 +53,88 @@ export class AuthGuard implements CanActivate {
     this.detectResetPswRoute();
     this.canActivate();
 
+    // router.events.subscribe((val) => {
+    //   // see also RoutesRecognized
+    //   // console.log('AUTH GUARD - NavigationEnd ', val instanceof NavigationEnd);
+    //   console.log('AUTH GUARD - RoutesRecognized ', val instanceof RoutesRecognized)
+    // });
+
+    // this.router.events.pairwise().subscribe((event) => {
+    //   console.log('-» -» -» AUTH GUARD EVENT ', event);
+    // });
+
+
+    this.getCurrentProject();
+    this.getProjectIdFromUrl();
+
+
+
+
+  }
+
+  getProjectIdFromUrl() {
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        // console.log('!! AUTH GUARD - EVENT ', e);
+        const current_url = e.url
+        console.log('!! AUTH GUARD - CURRENT URL ', current_url);
+
+        const url_segments = current_url.split('/');
+        console.log('!! AUTH GUARD - CURRENT URL SEGMENTS ', url_segments);
+
+        this.nav_project_id = url_segments[2];
+        console.log('!! AUTH GUARD - CURRENT URL SEGMENTS > NAVIGATION PROJECT ID: ', this.nav_project_id);
+
+        /**
+         * *** CHECK IF THE PROJECT ID GET FROM THE CURRENT URL IS THE SAME OF THE CURRENT PROJECT ***
+         * THE ID OF THE CURRENT PROJECT MAY BE DIFFERENT FROM THE ID OF THE PROJECT RETURNS FROM
+         * THE URL (the navigation project id) FOR EXAMPLE IN CASE THE USER ACCESSES A PAGE OF THE
+         * DASHBOARD FROM A LINK PRESENT IN THE EMAIL OF A REQUEST OR IN THE EMAIL OF INVITATION
+         * TO A NEW PROJECT
+         */
+        this.checkIf_NavPrjctIdMatchesCurrentPrjctId();
+      }
+    });
+  }
+
+  getCurrentProject() {
+    this.auth.project_bs.subscribe((project) => {
+      console.log('!! AUTH GUARD - CURRENT PROJECT: ', project)
+
+      if (project) {
+        console.log('!! AUTH GUARD - CURRENT PROJECT ID : ', project._id)
+        this.current_project_id = project._id;
+      }
+    });
+  }
+
+  checkIf_NavPrjctIdMatchesCurrentPrjctId() {
+
+    if (this.nav_project_id && this.current_project_id && this.nav_project_id === this.current_project_id) {
+      console.log('!!!!!! OOOOOK - AUTH GUARD - NAVIGATION-PROJECT-ID MATCHES CURRENT-PROJECT-ID')
+    } else {
+      console.log('!!!!!! KKKKKO - AUTH GUARD - NAVIGATION-PROJECT-ID DO NOT MATCHES CURRENT-PROJECT-ID')
+      // this.getProjectById();
+    }
+  }
+
+  /**
+   * *** GET PROJECT OBJECT BY ID ***
+   */
+  getProjectById() {
+    this.projectService.getMongDbProjectById(this.nav_project_id).subscribe((project: any) => {
+      console.log('++ > GET PROJECT (DETAILS) BY ID - PROJECT OBJECT: ', project);
+    },
+      (error) => {
+        console.log('GET PROJECT BY ID - ERROR ', error);
+      },
+      () => {
+        console.log('GET PROJECT BY ID - COMPLETE ');
+
+      });
   }
 
   detectVerifyEmailRoute() {
-
     // this.router.events.subscribe((val) => {
     if (this.location.path() !== '') {
       this.route = this.location.path();
@@ -117,6 +204,8 @@ export class AuthGuard implements CanActivate {
     //   return false;
     // }
   }
+
+
 
 
   // canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
