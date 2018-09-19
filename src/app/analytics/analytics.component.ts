d65c5cd67aa0c17a6c50be981ea8aaea2ce54ef9
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-09-19 18:20:22 +0200

    ANALYTICS: added the card 'last month requests count'

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -29,6 +29,7 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
   id_project: any;
   subscriptionToRequestService_RequestForAgent: Subscription;
   subscriptionToRequestService_RequestsCount: Subscription;
+  lastMonthrequestsCount: number;
   constructor(
     private auth: AuthService,
     private requestsService: RequestsService,
@@ -59,112 +60,107 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
     //   });
 
     // !!!!!! COMMEMNTO DA QUI SINO A TILEDESK ANALYTICS
-    const dataPreferences = {
-      labels: ['35.03%', '32.12%', '19.99%', '6.24%'],
-      series: [35.03, 32.12, 19.99, 6.24]
-    };
+    // const dataPreferences = {
+    //   labels: ['35.03%', '32.12%', '19.99%', '6.24%'],
+    //   series: [35.03, 32.12, 19.99, 6.24]
+    // };
 
-    const optionsPreferences = {
-      height: '230px'
-    };
+    // const optionsPreferences = {
+    //   height: '230px'
+    // };
 
-    // tslint:disable-next-line:no-unused-expression
-    new Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-
-
-    /* ----------==========   REQUESTS BY DAY  ==========---------- */
-
+    // // tslint:disable-next-line:no-unused-expression
+    // new Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
 
 
     /* ----------==========    NUMBER OF REQUEST   ==========---------- */
-    const dataDailySalesChart: any = {
-      // labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-      labels: ['V', 'S', 'D', 'L', 'M', 'M', 'G'],
-      series: [
-        [12, 17, 7, 17, 23, 18, 38]
-      ]
-    };
+    // const dataDailySalesChart: any = {
+    //   // labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    //   labels: ['V', 'S', 'D', 'L', 'M', 'M', 'G'],
+    //   series: [
+    //     [12, 17, 7, 17, 23, 18, 38]
+    //   ]
+    // };
 
-    const optionsDailySalesChart: any = {
-      lineSmooth: Chartist.Interpolation.cardinal({
-        tension: 0
-      }),
-      low: 0,
-      high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
-    }
+    // const optionsDailySalesChart: any = {
+    //   lineSmooth: Chartist.Interpolation.cardinal({
+    //     tension: 0
+    //   }),
+    //   low: 0,
+    //   high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    //   chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
+    // }
 
-    const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+    // const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
-    this.startAnimationForLineChart(dailySalesChart);
+    // this.startAnimationForLineChart(dailySalesChart);
 
 
     /* ----------==========     Users satisfaction    ==========---------- */
-    const dataCompletedTasksChart: any = {
-      // labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-      // labels: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
-      labels: ['Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic', 'Gen', 'Feb', 'Mar', 'Apr', 'Mag'],
-      series: [
-        [50, 60, 100, 45, 40, 65, 70, 91, 70, 75, 95, 98]
-      ]
-    };
+    // const dataCompletedTasksChart: any = {
+    //   // labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
+    //   // labels: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+    //   labels: ['Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic', 'Gen', 'Feb', 'Mar', 'Apr', 'Mag'],
+    //   series: [
+    //     [50, 60, 100, 45, 40, 65, 70, 91, 70, 75, 95, 98]
+    //   ]
+    // };
 
-    const optionsCompletedTasksChart: any = {
-      lineSmooth: Chartist.Interpolation.cardinal({
-        tension: 0
-      }),
-      low: 0,
-      high: 150, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 }
-    }
+    // const optionsCompletedTasksChart: any = {
+    //   lineSmooth: Chartist.Interpolation.cardinal({
+    //     tension: 0
+    //   }),
+    //   low: 0,
+    //   high: 150, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    //   chartPadding: { top: 0, right: 0, bottom: 0, left: 0 }
+    // }
 
-    const completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
+    // const completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
 
-    // start animation for the Completed Tasks Chart - Line Chart
-    this.startAnimationForLineChart(completedTasksChart);
+    // // start animation for the Completed Tasks Chart - Line Chart
+    // this.startAnimationForLineChart(completedTasksChart);
 
 
 
     /* ----------==========    Chat average duration    ==========---------- */
+    // const dataEmailsSubscriptionChart = {
+    //   // labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    //   // labels: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+    //   labels: ['Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic', 'Gen', 'Feb', 'Mar', 'Apr', 'Mag'],
+    //   series: [
+    //     [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 360]
 
-    const dataEmailsSubscriptionChart = {
-      // labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      // labels: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
-      labels: ['Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic', 'Gen', 'Feb', 'Mar', 'Apr', 'Mag'],
-      series: [
-        [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 360]
+    //   ]
+    // };
+    // const optionsEmailsSubscriptionChart = {
+    //   axisX: {
+    //     showGrid: false
+    //   },
+    //   low: 0,
+    //   high: 1000,
+    //   chartPadding: { top: 0, right: 5, bottom: 0, left: 0 }
+    // };
+    // const responsiveOptions: any[] = [
+    //   ['screen and (max-width: 640px)', {
+    //     seriesBarDistance: 5,
+    //     axisX: {
+    //       labelInterpolationFnc: function (value) {
+    //         return value[0];
+    //       }
+    //     }
+    //   }]
+    // ];
+    // const emailsSubscriptionChart = new Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
 
-      ]
-    };
-    const optionsEmailsSubscriptionChart = {
-      axisX: {
-        showGrid: false
-      },
-      low: 0,
-      high: 1000,
-      chartPadding: { top: 0, right: 5, bottom: 0, left: 0 }
-    };
-    const responsiveOptions: any[] = [
-      ['screen and (max-width: 640px)', {
-        seriesBarDistance: 5,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value[0];
-          }
-        }
-      }]
-    ];
-    const emailsSubscriptionChart = new Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
-
-    // start animation for the Emails Subscription Chart
-    this.startAnimationForBarChart(emailsSubscriptionChart);
+    // // start animation for the Emails Subscription Chart
+    // this.startAnimationForBarChart(emailsSubscriptionChart);
 
     /* ----------==========   TILEDESK ANALYTICS   ==========---------- */
     this.getCurrentProject();
     // this.getCountOfRequestForAgent()
     this.getAllUsersOfCurrentProject();
     this.getRequestsByDay();
-
+    this.getlastMonthRequetsCount();
   }
   /* ----------==========   end ON INIT    ==========---------- */
   getRequestsByDay() {
@@ -216,13 +212,12 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
 
     }, (error) => {
       console.log('!!! ANALYTICS - REQUESTS BY DAY - ERROR ', error);
-    },
-      () => {
-        console.log('!!! ANALYTICS - REQUESTS BY DAY * COMPLETE *');
-      });
+    }, () => {
+      console.log('!!! ANALYTICS - REQUESTS BY DAY * COMPLETE *');
+    });
   }
-  getMaxOfArray(requestsByDay_series_array) {
 
+  getMaxOfArray(requestsByDay_series_array) {
     return Math.max.apply(null, requestsByDay_series_array);
   }
 
@@ -360,7 +355,6 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
   }
 
 
-
   getOccurrenceAndAssignToProjectUsers(array, value) {
     console.log('!!! ANALYTICS - !!!!! CALLING GET OCCURRENCE AND ASSIGN TO PROJECT USERS');
     let count = 0;
@@ -421,6 +415,18 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
     });
   }
 
+  getlastMonthRequetsCount() {
+    this.requestsService.lastMonthRequetsCount().subscribe((_lastMonthrequestsCount: any) => {
+      console.log('!!! ANALYTICS - LAST MONTH REQUESTS COUNT - RESPONSE ', _lastMonthrequestsCount);
+      this.lastMonthrequestsCount = _lastMonthrequestsCount[0]['totalCount'];
+
+    }, (error) => {
+      console.log('!!! ANALYTICS - LAST MONTH REQUESTS COUNT - ERROR ', error);
+    }, () => {
+      console.log('!!! ANALYTICS - LAST MONTH REQUESTS COUNT * COMPLETE *');
+    });
+  }
+
   goToMemberProfile(member_id: string) {
     this.router.navigate(['project/' + this.id_project + '/member/' + member_id]);
   }
@@ -432,7 +438,7 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
     this.subscriptionToRequestService_RequestForAgent.unsubscribe();
   }
 
-  // !!!!! COMMENTO DA QUI
+
   startAnimationForLineChart(chart) {
     let seq: any, delays: any, durations: any;
     seq = 0;
@@ -466,29 +472,31 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
 
     seq = 0;
   };
-  startAnimationForBarChart(chart) {
-    let seq2: any, delays2: any, durations2: any;
 
-    seq2 = 0;
-    delays2 = 80;
-    durations2 = 500;
-    chart.on('draw', function (data) {
-      if (data.type === 'bar') {
-        seq2++;
-        data.element.animate({
-          opacity: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: 0,
-            to: 1,
-            easing: 'ease'
-          }
-        });
-      }
-    });
+    // !!!!! COMMENTO DA QUI
+  // startAnimationForBarChart(chart) {
+  //   let seq2: any, delays2: any, durations2: any;
 
-    seq2 = 0;
-  };
+  //   seq2 = 0;
+  //   delays2 = 80;
+  //   durations2 = 500;
+  //   chart.on('draw', function (data) {
+  //     if (data.type === 'bar') {
+  //       seq2++;
+  //       data.element.animate({
+  //         opacity: {
+  //           begin: seq2 * delays2,
+  //           dur: durations2,
+  //           from: 0,
+  //           to: 1,
+  //           easing: 'ease'
+  //         }
+  //       });
+  //     }
+  //   });
+
+  //   seq2 = 0;
+  // };
 
 
 }
