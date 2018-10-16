e918351d158bf4e79b2482716c17dde7b97167bb
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-10-16 16:16:00 +0200

    WIDGET DESIGN:  start of the implementation

--- src/app/widget-design/widget-design.component.ts
+++ src/app/widget-design/widget-design.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { slideInOutAnimation } from './slide-in-out.animation';
 @Component({
   selector: 'appdashboard-widget-design',
   templateUrl: './widget-design.component.html',
-  styleUrls: ['./widget-design.component.scss'],
-  animations: [slideInOutAnimation],
-  host: { '[@slideInOutAnimation]': '' }
+  styleUrls: ['./widget-design.component.scss']
 })
 
 
 export class WidgetDesignComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public location: Location
+  ) { }
 
   ngOnInit() {
-    
+
   }
+
+
+  goBack() {
+    this.location.back();
+  }
+
 
 }
