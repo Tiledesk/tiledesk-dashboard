c5e3d6ea1aadce2ea307bd8200e2b01e286c7d8f
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-11-06 19:44:38 +0100

    Corrected TEST WIDGET + translated widget design

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { WidgetService } from '../services/widget.service';
 import { NotifyService } from '../core/notify.service';
 import { ProjectService } from '../services/project.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-widget',
@@ -30,6 +31,7 @@ export class WidgetComponent implements OnInit {
   http: Http;
   // calloutTimerSecondSelected = -1;
   calloutTimerSecondSelected: any;
+  paramCalloutTimer: string;
 
   // preChatForm = 'preChatForm'
   calloutTimerOptions = [
@@ -72,18 +74,26 @@ export class WidgetComponent implements OnInit {
   logoChatValue: string;
   wellcomeTitleValue: string;
   wellcomeMsgValue: string;
+
+  calloutTimerSecondSelectedValue: any;
+  alignmentValue: any;
+  browserLang: string;
+
+  callOutMsgValue: string;
+
   constructor(
     http: Http,
     private auth: AuthService,
     private router: Router,
     private widgetService: WidgetService,
     private notify: NotifyService,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private translate: TranslateService
   ) { this.http = http }
 
   ngOnInit() {
     this.auth.checkRoleForCurrentProject();
-
+    this.getBrowserLang();
     this.getCurrentProject();
 
     // this.subscribeToSelectedPrimaryColor();
@@ -96,6 +106,10 @@ export class WidgetComponent implements OnInit {
     console.log('**** ON INIT ALIGNMENT SELECTED ', this.alignmentSelected)
   }
 
+  getBrowserLang() {
+    this.browserLang = this.translate.getBrowserLang();
+    console.log('WIDGET DESIGN - BROWSER LANG ', this.browserLang)
+  }
   getCurrentProject() {
     this.auth.project_bs
       .subscribe((project) => {
@@ -125,23 +139,25 @@ export class WidgetComponent implements OnInit {
 
 
         if (project.widget.wellcomeTitle) {
-          this.wellcomeTitleValue = `\n      wellcomeTitle: "${project.widget.wellcomeTitle}",`
+          // tslint:disable-next-line:max-line-length
+          this.wellcomeTitleValue = `\n      wellcomeTitle: "${project.widget.wellcomeTitle.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0')}",`
           console.log('»» WIDGET - PRJCT-WIDGET WELCOME TITLE : ', this.wellcomeTitleValue);
         }
 
         if (project.widget.wellcomeMsg) {
-          this.wellcomeMsgValue = `\n      wellcomeTitle: "${project.widget.wellcomeMsg}",`
+          // tslint:disable-next-line:max-line-length
+          this.wellcomeMsgValue = `\n      wellcomeTitle: "${project.widget.wellcomeMsg.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0')}",`
           console.log('»» WIDGET - PRJCT-WIDGET WELCOME MSG : ', this.wellcomeMsgValue);
         }
 
         if (project.widget.logoChat) {
           this.logoChatValue = `\n      logoChat: "${project.widget.logoChat}",`
-          console.log('»» WIDGET - PRJCT-WIDGET LOGO CHAT : ', this.calloutTimerSecondSelected);
+          console.log('»» WIDGET - PRJCT-WIDGET LOGO CHAT : ', this.logoChatValue);
         }
 
         if (project.widget.preChatForm) {
           this.preChatFormValue = `\n      preChatForm: ${project.widget.preChatForm},`
-          console.log('»» WIDGET - PRJCT-WIDGET PRECHAT FORM : ', this.calloutTimerSecondSelected);
+          console.log('»» WIDGET - PRJCT-WIDGET PRECHAT FORM : ', this.preChatFormValue);
           this.preChatForm = true;
         } else {
           this.preChatForm = false;
@@ -149,36 +165,70 @@ export class WidgetComponent implements OnInit {
 
         if (project.widget.calloutTimer) {
           this.calloutTimerSecondSelected = `\n      calloutTimer: ${project.widget.calloutTimer},`
+          this.calloutTimerSecondSelectedValue = project.widget.calloutTimer;
           console.log('»» WIDGET - PRJCT-WIDGET CALLOUT TIMER : ', this.calloutTimerSecondSelected);
         }
 
         if (project.widget.calloutTitle) {
-          this.calloutTitle = `\n      calloutTitle: "${project.widget.calloutTitle}",`
+          this.calloutTitle = `\n      calloutTitle: "${project.widget.calloutTitle.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0')}",`
+          // used for TEST WIDGET
+          this.escaped_calloutTitle = project.widget.calloutTitle.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
           console.log('»» WIDGET - PRJCT-WIDGET CALLOUT TITLE : ', this.calloutTitle);
         }
 
         if (project.widget.calloutMsg) {
-          this.calloutMsg = `\n      calloutMsg: "${project.widget.calloutMsg}",`
+          this.calloutMsg = `\n      calloutMsg: "${project.widget.calloutMsg.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0')}",`;
+          // used for TEST WIDGET
+          this.escaped_calloutMsg = project.widget.calloutMsg.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
           console.log('»» WIDGET - PRJCT-WIDGET CALLOUT MSG : ', this.calloutMsg);
         }
 
         if (project.widget.align) {
           this.alignmentSelected = `\n      align: "${project.widget.align}",`
+          // used for TEST WIDGET
+          this.alignmentValue = project.widget.align
           console.log('»» WIDGET - PRJCT-WIDGET ALIGNMENT : ', this.alignmentSelected);
+        } else {
+          this.alignmentValue = 'right'
         }
 
         if (project.widget.themeColor) {
-          this.themeColor = `\n      themeColor: "${project.widget.themeColor}",`
+          this.themeColor = `\n      themeColor: "${project.widget.themeColor}",`;
+          // used for TEST WIDGET
+          this.primaryColor = this.hexToRgb(project.widget.themeColor);
         }
 
         if (project.widget.themeForegroundColor) {
-          this.themeForegroundColor = `\n      themeForegroundColor: "${project.widget.themeForegroundColor}",`
+          this.themeForegroundColor = `\n      themeForegroundColor: "${project.widget.themeForegroundColor}",`;
+          // used for TEST WIDGET
+          this.secondaryColor = this.hexToRgb(project.widget.themeForegroundColor);
         }
       }
 
     });
   }
 
+  hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    const resultR = parseInt(result[1], 16);
+    const resultG = parseInt(result[2], 16);
+    const resultB = parseInt(result[3], 16)
+
+    return result ? 'rgb' + '(' + resultR + ',' + resultG + ',' + resultB + ')' : null;
+
+    // return result ? {
+    //   r: parseInt(result[1], 16),
+    //   g: parseInt(result[2], 16),
+    //   b: parseInt(result[3], 16)
+    // } : null;
+  }
 
   subscribeToWidgetAlignment() {
     this.widgetService.widgetAlignmentBs
@@ -206,38 +256,38 @@ export class WidgetComponent implements OnInit {
   /**
    * TO AVOID THE CALLOUT TIMER EMPTY WHEN THE USER, AFTER HAVING SELECTED THE CALLOUT TIMER,
    * CHANGE PAGE AND THEN RETURN IN THE WIDGET PAGE (SEE  publishCalloutTitle) */
-  subscribeToSelectedCalloutTimer() {
-    this.widgetService.calloutTimerBs
-      .subscribe((timer_selected: number) => {
-        console.log('WIDGET COMP - SUBSCRIBE TO CALLOUT TIMER ', timer_selected);
-        if (timer_selected) {
-          this.calloutTimerSecondSelected = timer_selected
-        }
-      });
-  }
+  // subscribeToSelectedCalloutTimer() {
+  //   this.widgetService.calloutTimerBs
+  //     .subscribe((timer_selected: number) => {
+  //       console.log('WIDGET COMP - SUBSCRIBE TO CALLOUT TIMER ', timer_selected);
+  //       if (timer_selected) {
+  //         this.calloutTimerSecondSelected = timer_selected
+  //       }
+  //     });
+  // }
 
   /**
    * TO AVOID THE CALLOUT TITLE EMPTY WHEN THE USER, AFTER HAVING TYPED THE CALLOUT TITLE, 
    * CHANGE PAGE AND THEN RETURN IN THE WIDGET PAGE (SEE  publishCalloutTimerSelected) */
-  subscribeToTypedCalloutTitle() {
-    this.widgetService.calloutTitleBs
-      .subscribe((title_typed: string) => {
-        console.log('WIDGET COMP - SUBSCRIBE TO CALLOUT TITLE ', title_typed);
+  // subscribeToTypedCalloutTitle() {
+  //   this.widgetService.calloutTitleBs
+  //     .subscribe((title_typed: string) => {
+  //       console.log('WIDGET COMP - SUBSCRIBE TO CALLOUT TITLE ', title_typed);
 
-        if (title_typed) {
-          this.calloutTitleText = title_typed;
-          this.escaped_calloutTitle = this.calloutTitleText.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-          console.log('+++ +++ ON SUBSCRIBE CALLOUT TITLE TEXT  ', this.calloutTitleText);
-          console.log('+++ +++ ON SUBSCRIBE ESCAPED CALLOUT TITLE ', this.escaped_calloutTitle);
-          this.calloutTitle = `\n      calloutTitle: "${this.escaped_calloutTitle}",` // is used in the texarea 'script'
-        } else {
-          console.log('+++ +++ ON SUBSCRIBE CALLOUT TITLE TEXT (else) ', this.calloutTitleText);
-          this.escaped_calloutTitle = '';
-          this.calloutTitle = '';
-        }
+  //       if (title_typed) {
+  //         this.calloutTitleText = title_typed;
+  //         this.escaped_calloutTitle = this.calloutTitleText.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+  //         console.log('+++ +++ ON SUBSCRIBE CALLOUT TITLE TEXT  ', this.calloutTitleText);
+  //         console.log('+++ +++ ON SUBSCRIBE ESCAPED CALLOUT TITLE ', this.escaped_calloutTitle);
+  //         this.calloutTitle = `\n      calloutTitle: "${this.escaped_calloutTitle}",` // is used in the texarea 'script'
+  //       } else {
+  //         console.log('+++ +++ ON SUBSCRIBE CALLOUT TITLE TEXT (else) ', this.calloutTitleText);
+  //         this.escaped_calloutTitle = '';
+  //         this.calloutTitle = '';
+  //       }
 
-      });
-  }
+  //     });
+  // }
 
   subscribeToTypedCalloutMsg() {
     this.widgetService.calloutMsgBs
@@ -259,53 +309,53 @@ export class WidgetComponent implements OnInit {
       });
   }
 
-  subscribeToSelectedPrimaryColor() {
-    this.widgetService.primaryColorBs
-      .subscribe((primary_color: string) => {
-        console.log('WIDGET COMP - SUBSCRIBE TO PRIMARY COLOR ', primary_color);
-        if (primary_color) {
-          this.primaryColor = primary_color
+  // subscribeToSelectedPrimaryColor() {
+  //   this.widgetService.primaryColorBs
+  //     .subscribe((primary_color: string) => {
+  //       console.log('WIDGET COMP - SUBSCRIBE TO PRIMARY COLOR ', primary_color);
+  //       if (primary_color) {
+  //         this.primaryColor = primary_color
 
-          // this.themeColor IS THE PROPERTY USED IN THE TEXTAREA 'script'
-          this.themeColor = `\n      themeColor: "${this.primaryColor}",`
-        }
-      });
-  }
+  //         // this.themeColor IS THE PROPERTY USED IN THE TEXTAREA 'script'
+  //         this.themeColor = `\n      themeColor: "${this.primaryColor}",`
+  //       }
+  //     });
+  // }
 
-  subscribeToSelectedSecondaryColor() {
-    this.widgetService.secondaryColorBs
-      .subscribe((secondary_color: string) => {
-        console.log('WIDGET COMP - SECONDARY COLOR ', secondary_color);
-        if (secondary_color) {
-          this.secondaryColor = secondary_color
+  // subscribeToSelectedSecondaryColor() {
+  //   this.widgetService.secondaryColorBs
+  //     .subscribe((secondary_color: string) => {
+  //       console.log('WIDGET COMP - SECONDARY COLOR ', secondary_color);
+  //       if (secondary_color) {
+  //         this.secondaryColor = secondary_color
 
-          // this.themeForegroundColor IS THE PROPERTY USED IN THE TEXTAREA 'script'
-          this.themeForegroundColor = `\n      themeForegroundColor: "${this.secondaryColor}",`
-        }
-      });
-  }
+  //         // this.themeForegroundColor IS THE PROPERTY USED IN THE TEXTAREA 'script'
+  //         this.themeForegroundColor = `\n      themeForegroundColor: "${this.secondaryColor}",`
+  //       }
+  //     });
+  // }
 
-  onKeyCalloutTitle() {
-    if (this.calloutTitleText) {
-      this.escaped_calloutTitle = this.calloutTitleText.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-      console.log('+++ +++ ON KEY-UP CALLOUT TITLE TEXT  ', this.calloutTitleText);
-      console.log('+++ +++ ON KEY-UP ESCAPED CALLOUT TITLE ', this.escaped_calloutTitle);
+  // onKeyCalloutTitle() {
+  //   if (this.calloutTitleText) {
+  //     this.escaped_calloutTitle = this.calloutTitleText.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+  //     console.log('+++ +++ ON KEY-UP CALLOUT TITLE TEXT  ', this.calloutTitleText);
+  //     console.log('+++ +++ ON KEY-UP ESCAPED CALLOUT TITLE ', this.escaped_calloutTitle);
 
-      /**
-       * THE calloutTitleText IS PASSED TO THE WIDGET SERVICE THAT PUBLISH the property calloutTitleBs
-       * THIS SAME COMP SUBSCRIBES TO THE WIDGET SERVICE (see subscribeToTypedCalloutTitle) TO AVOID THAT,
-       * WHEN THE USER GO TO THE WIDEGT DESIGN PAGE (or in another page) AND THEN RETURN IN THE
-       * WIDGET PAGE, THE VALUE OF THE CALLOUT TITLE BE EMPTY EVEN IF HE HAD PREVIOUSLY DIGITED IT */
-      this.widgetService.publishCalloutTitleTyped(this.calloutTitleText)
+  //     /**
+  //      * THE calloutTitleText IS PASSED TO THE WIDGET SERVICE THAT PUBLISH the property calloutTitleBs
+  //      * THIS SAME COMP SUBSCRIBES TO THE WIDGET SERVICE (see subscribeToTypedCalloutTitle) TO AVOID THAT,
+  //      * WHEN THE USER GO TO THE WIDEGT DESIGN PAGE (or in another page) AND THEN RETURN IN THE
+  //      * WIDGET PAGE, THE VALUE OF THE CALLOUT TITLE BE EMPTY EVEN IF HE HAD PREVIOUSLY DIGITED IT */
+  //     this.widgetService.publishCalloutTitleTyped(this.calloutTitleText)
 
-      this.calloutTitle = `\n      calloutTitle: "${this.escaped_calloutTitle}",` // is used in the texarea 'script'
-    } else {
-      console.log('+++ +++ ON KEY-UP CALLOUT TITLE TEXT (else) ', this.calloutTitleText);
-      this.widgetService.publishCalloutTitleTyped(this.calloutTitleText)
-      this.escaped_calloutTitle = '';
-      this.calloutTitle = '';
-    }
-  }
+  //     this.calloutTitle = `\n      calloutTitle: "${this.escaped_calloutTitle}",` // is used in the texarea 'script'
+  //   } else {
+  //     console.log('+++ +++ ON KEY-UP CALLOUT TITLE TEXT (else) ', this.calloutTitleText);
+  //     this.widgetService.publishCalloutTitleTyped(this.calloutTitleText)
+  //     this.escaped_calloutTitle = '';
+  //     this.calloutTitle = '';
+  //   }
+  // }
 
   onKeyCalloutMsg() {
     if (this.calloutMsgText) {
@@ -391,31 +441,74 @@ export class WidgetComponent implements OnInit {
   }
 
 
+
+
   testWidgetPage() {
     this.elementRef.nativeElement.blur();
     // http://testwidget.tiledesk.com/testsite/?projectid=5ad069b123c415001469574f&prechatform=false
     // + '&projectname=' + this.projectName
     // tslint:disable-next-line:max-line-length
 
+    console.log('TEST WIDGET CALL OUT TIMER VALUE ', this.calloutTimerSecondSelectedValue);
+
+    if (this.calloutTimerSecondSelectedValue) {
+      this.paramCalloutTimer = '&callout_timer=' + this.calloutTimerSecondSelectedValue;
+    } else {
+      this.paramCalloutTimer = '&callout_timer=' + -1;
+    }
+
     let calloutTitle = this.escaped_calloutTitle
     let paramCallout_title = '&callout_title='
 
+
     console.log('CALL OUT TITLE PARAMETER ', paramCallout_title, 'CALL OUT TITLE VALUE  ', calloutTitle);
-    if (!this.escaped_calloutTitle) {
+    if (!this.escaped_calloutTitle && !this.escaped_calloutMsg) {
       paramCallout_title = '';
       calloutTitle = '';
       console.log('CALL OUT TITLE PARAMETER ', paramCallout_title, 'CALL OUT TITLE VALUE  ', calloutTitle);
     }
 
+    if (!this.escaped_calloutTitle && this.escaped_calloutMsg) {
+      paramCallout_title = '&callout_title='
+
+      if (this.browserLang) {
+        if (this.browserLang === 'it') {
+          calloutTitle = 'Bisogno di aiuto?';
+        } else {
+          calloutTitle = 'Need Help?';
+        }
+      }
+      console.log('CALL OUT TITLE PARAMETER ', paramCallout_title, 'CALL OUT TITLE VALUE  ', calloutTitle);
+    }
+
+
+
+
     let calloutMsg = this.escaped_calloutMsg;
     let paramCallout_msg = '&callout_msg='
 
     console.log('CALL OUT MSG PARAMETER ', paramCallout_msg, 'CALL OUT MSG VALUE  ', calloutMsg);
-    if (!this.escaped_calloutMsg) {
+    if (!this.escaped_calloutTitle && !this.escaped_calloutMsg) {
       paramCallout_msg = '';
       calloutMsg = ''
       console.log('CALL OUT MSG PARAMETER ', paramCallout_msg, 'CALL OUT MSG VALUE  ', calloutMsg);
     }
+
+    if (this.escaped_calloutTitle && !this.escaped_calloutMsg) {
+      console.log('TEST WIDGET CALL OUT TITLE ', this.escaped_calloutTitle, 'CALL OUT MSG ', this.escaped_calloutMsg);
+
+      paramCallout_msg = '&callout_msg='
+
+      if (this.browserLang) {
+        if (this.browserLang === 'it') {
+          calloutMsg = 'Clicca qui e inizia a chattare con noi!';
+        } else {
+          calloutMsg = 'Click here and start chatting with us!';
+        }
+      }
+    }
+
+
 
     let paramThemeColor = '&themecolor=' + this.primaryColor
     if (!this.primaryColor) {
@@ -432,14 +525,15 @@ export class WidgetComponent implements OnInit {
     const url = 'http://testwidget.tiledesk.com/testsite?projectid='
       + this.projectId
       + '&prechatform=' + this.preChatForm
-      + '&callout_timer=' + this.calloutTimerSecondSelected
+      + this.paramCalloutTimer
       + paramThemeColor
       + paramThemeforegroundcolor
       + paramCallout_title + calloutTitle
       + paramCallout_msg + calloutMsg
-      + '&align=' + this.alignmentSelected;
+      + '&align=' + this.alignmentValue;
     window.open(url, '_blank');
   }
+
 
   goToWidgetDesign() {
     this.router.navigate(['project/' + this.project._id + '/widget/design']);
