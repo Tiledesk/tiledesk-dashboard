8a19cf8697388d13728c09144ecd7d48493da7de
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-11-15 13:08:45 +0100

    CREATED the model pending-invitatio

---
+++ src/app/models/pending-invitation-model.ts
@@ -0,0 +1,10 @@
+export interface PendingInvitation {
+    _id: string;
+    updatedAt: any;
+    createdAt: any;
+    email: string;
+    role: string;
+    id_project: string;
+    createdBy: string;
+    __v: any;
+}
