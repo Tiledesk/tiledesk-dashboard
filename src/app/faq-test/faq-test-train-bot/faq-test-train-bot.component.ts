ca966820bcd972405d3c5a4fe7cb11b22bc7e149
nicolan74 <nicola.lanzilotto@frontiere21.it>
2019-05-10 16:29:32 +0200

    only the first "answer bubble" coloured + other minor improvements

--- src/app/faq-test/faq-test-train-bot/faq-test-train-bot.component.ts
+++ src/app/faq-test/faq-test-train-bot/faq-test-train-bot.component.ts
@@ -18,7 +18,7 @@ export class FaqTestTrainBotComponent implements OnInit {
   @Input() selectedQuestion: string;
   @Input() remote_faq_kb_key: string;
   @Input() idBot: string;
-  
+
   hits: any;
   showSpinner = true;
   answer: string;
@@ -40,35 +40,35 @@ export class FaqTestTrainBotComponent implements OnInit {
     this.translateAnswerSuccessfullyAdded();
   }
 
-  
 
-  translateAnswerSuccessfullyAdded(){
+
+  translateAnswerSuccessfullyAdded() {
 
     this.translate.get('AnswerSuccessfullyAdded')
-    .subscribe((text: string) => {
+      .subscribe((text: string) => {
 
-      this.answerSuccessfullyAdded = text;
-      console.log('FaqTestTrainBotComponent + + + AnswerSuccessfullyAdded', text)
-    });
+        this.answerSuccessfullyAdded = text;
+        console.log('FaqTestTrainBotComponent + + + AnswerSuccessfullyAdded', text)
+      });
   }
 
   searchRemoteFaq() {
-   
+
     this.faqService.searchRemoteFaqByRemoteFaqKbKey(this.remote_faq_kb_key, this.selectedQuestion)
-    .subscribe((remoteFaq) => {
-      console.log('FaqTestTrainBotComponent - REMOTE FAQ FOUND - POST DATA ', remoteFaq);
+      .subscribe((remoteFaq) => {
+        console.log('FaqTestTrainBotComponent - REMOTE FAQ FOUND - POST DATA ', remoteFaq);
 
-      if (remoteFaq) {
-        this.hits = remoteFaq.hits
-      }
+        if (remoteFaq) {
+          this.hits = remoteFaq.hits
+        }
 
-    }, (error) => {
-      console.log('FaqTestTrainBotComponent - REMOTE FAQ - POST REQUEST ERROR ', error);
-      this.showSpinner = false;
-    }, () => {
-      console.log('FaqTestTrainBotComponent - REMOTE FAQ - POST REQUEST * COMPLETE *');
-      this.showSpinner = false;
-    });
+      }, (error) => {
+        console.log('FaqTestTrainBotComponent - REMOTE FAQ - POST REQUEST ERROR ', error);
+        this.showSpinner = false;
+      }, () => {
+        console.log('FaqTestTrainBotComponent - REMOTE FAQ - POST REQUEST * COMPLETE *');
+        this.showSpinner = false;
+      });
 
   }
 
@@ -78,30 +78,36 @@ export class FaqTestTrainBotComponent implements OnInit {
    */
   create() {
     console.log('MONGO DB CREATE FAQ - QUESTION: ', this.selectedQuestion, ' - ANSWER: ', this.answer, ' - ID FAQ KB ', this.idBot);
-    this.faqService.addMongoDbFaq(this.selectedQuestion, this.answer, this.idBot)
-      .subscribe((faq) => {
-        console.log('FaqTestComponent - CREATED FAQ ', faq);
 
-       
-      }, (error) => {
+    if (this.answer) {
+      this.faqService.addMongoDbFaq(this.selectedQuestion, this.answer, this.idBot)
+        .subscribe((faq) => {
+          console.log('FaqTestComponent - CREATED FAQ ', faq);
 
-        console.log('FaqTestComponent - CREATED FAQ - ERROR ', error);
-        // =========== NOTIFY ERROR ===========
-      
-        // this.notify.showNotification(this.createFaqErrorNoticationMsg, 4, 'report_problem');
-      }, () => {
-        console.log('FaqTestComponent - CREATED FAQ * COMPLETE *');
-        // =========== NOTIFY SUCCESS===========
-     
-        this.notify.showNotification(this.answerSuccessfullyAdded, 2, 'done');
 
-      //  SET A TIMEOUT TO AVOID THAT REMOTE FAQ ARE NOT UPDATED
-      this.showSpinner = true;
-      setTimeout(() => {
-        this.searchRemoteFaq();
-      }, 500);
+        }, (error) => {
 
-      });
+          console.log('FaqTestComponent - CREATED FAQ - ERROR ', error);
+          // =========== NOTIFY ERROR ===========
+
+          // this.notify.showNotification(this.createFaqErrorNoticationMsg, 4, 'report_problem');
+        }, () => {
+          console.log('FaqTestComponent - CREATED FAQ * COMPLETE *');
+          // =========== NOTIFY SUCCESS===========
+          this.answer = '';
+          this.notify.showNotification(this.answerSuccessfullyAdded, 2, 'done');
+
+          //  SET A TIMEOUT TO AVOID THAT REMOTE FAQ ARE NOT UPDATED
+          this.showSpinner = true;
+          setTimeout(() => {
+            this.searchRemoteFaq();
+          }, 500);
+
+        });
+    } else {
+
+      console.log('FaqTestComponent - CREATED FAQ - ANSWER', this.answer);
+    }
 
   }
 
@@ -110,7 +116,7 @@ export class FaqTestTrainBotComponent implements OnInit {
   closeRightSideBar() {
     console.log('FaqTestTrainBotComponent - closeRightSideBar ')
     this.valueChange.emit(false);
-    
+
 
 
     [].forEach.call(
