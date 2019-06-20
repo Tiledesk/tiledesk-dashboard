019b331b5a3e8ded9108adf0575c02b931c50c38
nicolan74 <nicola.lanzilotto@frontiere21.it>
2019-06-20 12:54:18 +0200

    bug fixed

--- src/app/pricing/payment-success-page/payment-success-page.component.ts
+++ src/app/pricing/payment-success-page/payment-success-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'appdashboard-payment-success-page',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentSuccessPageComponent implements OnInit {
 
-  constructor() { }
+  id_project: string;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+
+  getCurrentProject() {
+    this.auth.project_bs.subscribe((project) => {
+
+      if (project) {
+        this.id_project = project._id
+
+      }
+    });
+  }
+
+  goToHome() {
+
+    this.router.navigate(['/project/' + this.id_project + '/home']);
   }
 
 }
