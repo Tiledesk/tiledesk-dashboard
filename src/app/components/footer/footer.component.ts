af31679f81b034b6bfa3f8d212614e38f75b8a58
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-01-07 13:59:15 +0100

    Adds: get properties from 'brand' (in progress)

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import brand from 'assets/brand/brand.json';
 // declare var require: any;
 @Component({
   selector: 'app-footer',
@@ -10,11 +11,33 @@ export class FooterComponent implements OnInit {
   test: Date = new Date();
   // public version: string = require( '../../../../package.json').version;
   public version: string = environment.VERSION;
+  companySiteName = brand.company_site_name;
+  companySiteUrl = brand.company_site_url;
+  companyContactUsEmail = brand.company_contact_us_email;
+
+  privacyPolicyLinkName = brand.footer.privacy_policy_link_name
+  privacyPolicyUrl = brand.footer.privacy_policy_url
+  displayTermsAndConditionsLink = brand.footer.display_terms_and_conditions_link
+  termsAndConditionsUrl = brand.footer.terms_and_conditions_url
+
 
   constructor() { }
 
   ngOnInit() {
     console.log('version (footer.component)  ', this.version);
+    this.brandLog()
+  }
+
+  brandLog() {
+
+    console.log('BRAND_JSON - FOOTER ', brand);
+    console.log('BRAND_JSON - FOOTER companySiteName ', this.companySiteName);
+    console.log('BRAND_JSON - FOOTER companySiteUrl ', this.companySiteUrl);
+    console.log('BRAND_JSON - FOOTER companyContactUsEmail ', this.companyContactUsEmail);
+    console.log('BRAND_JSON - FOOTER privacyPolicyLinkName ', this.privacyPolicyLinkName);
+    console.log('BRAND_JSON - FOOTER privacyPolicyUrl ', this.privacyPolicyUrl);
+    console.log('BRAND_JSON - FOOTER displayTermsAndConditionsLink ', this.displayTermsAndConditionsLink);
+    console.log('BRAND_JSON - FOOTER termsAndConditionsUrl ', this.termsAndConditionsUrl);
   }
 
 }
