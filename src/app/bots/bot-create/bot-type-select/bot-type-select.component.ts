c2ded36f4fd0bda07b2b2550b9c64f0dbdd06952
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-02-14 19:46:28 +0100

    minor improvements

--- src/app/bots/bot-create/bot-type-select/bot-type-select.component.ts
+++ src/app/bots/bot-create/bot-type-select/bot-type-select.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/auth.service';
 import { Location } from '@angular/common';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'appdashboard-bot-type-select',
@@ -9,9 +10,9 @@ import { Location } from '@angular/common';
   styleUrls: ['./bot-type-select.component.scss']
 })
 export class BotTypeSelectComponent implements OnInit {
-
+  public_Key = environment.t2y12PruGU9wUtEGzBJfolMIgK;
   projectId: string;
-
+  dgfIsVisible: boolean;
   constructor(
     private router: Router,
     public location: Location,
@@ -20,18 +21,47 @@ export class BotTypeSelectComponent implements OnInit {
 
   ngOnInit() {
     this.getCurrentProject()
+    this.getOSCODE();
   }
 
 
 
   getCurrentProject() {
     this.auth.project_bs.subscribe((project) => {
-     
+
       if (project) {
         this.projectId = project._id;
       }
     });
   }
+
+
+  getOSCODE() {
+    let keys = this.public_Key.split("-");
+    console.log('PUBLIC-KEY (SIDEBAR) - public_Key keys', keys)
+
+    keys.forEach(key => {
+      // console.log('NavbarComponent public_Key key', key)
+      if (key.includes("DGF")) {
+        // console.log('PUBLIC-KEY (SIDEBAR) - key', key);
+        let dgf = key.split(":");
+        console.log('PUBLIC-KEY (BOT-TYPE-SELECT) - dgf key&value', dgf);
+
+        if (dgf[1] === "F") {
+          this.dgfIsVisible = false;
+          console.log('PUBLIC-KEY (BOT-TYPE-SELECT) - dgf dgfIsVisible', this.dgfIsVisible);
+        } else {
+          this.dgfIsVisible = true;
+          console.log('PUBLIC-KEY (BOT-TYPE-SELECT) - dgf dgfIsVisible', this.dgfIsVisible);
+        }
+      }
+
+    });
+
+  }
+
+
+
 
   goToCreateBot(type: string) {
     console.log('Bot Type Selected type ', type)
