702e140e1c64ae558940f12583b5a246504e3983
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-02-22 08:07:17 +0100

    modified to get all the 'app congif'

--- src/app/services/app-config.service.ts
+++ src/app/services/app-config.service.ts
@@ -20,13 +20,31 @@ export class AppConfigService {
     try {
       const data = await this.http.get(this.appConfig.remoteConfigUrl)
         .toPromise();
-      // console.log('AppConfigService loadAppConfig data: ', data['_body']);
-      this.appConfig.firebase = JSON.parse(data['_body']);
+      // console.log('AppConfigService loadAppConfig data: ', data['_body']['firebase']);
+      console.log('AppConfigService loadAppConfig data: ', data);
+
+      const dataObject = JSON.parse(data['_body'])
+      console.log('AppConfigService loadAppConfig data as Object: ', dataObject);
+
+
+
+      const firebaseConfig = dataObject['firebase'];
+      console.log('AppConfigService loadAppConfig firebaseConfig: ', firebaseConfig);
+
+      const allconfig = dataObject
+      console.log('AppConfigService loadAppConfig allconfig: ', allconfig);
+
+      // this.appConfig.firebase = JSON.parse(data['_body']);
+
+      // this.appConfig.firebase = firebaseConfig;
+      this.appConfig = allconfig;
+
     } catch (err) {
       console.log('AppConfigService loadAppConfig error : ', err);
     }
   }
 
+ 
   getConfig() {
     // console.log('AppConfigService getConfig ', this.appConfig);
     return this.appConfig;
