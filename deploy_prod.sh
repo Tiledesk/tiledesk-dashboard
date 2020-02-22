f5dc372cadb90a259fc2bfe47ec423c906ddff9f
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-02-22 08:24:02 +0100

    updated

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -11,9 +11,13 @@
 
 ng build --prod --base-href ./
 cd dist
-aws s3 sync . s3://tiledesk-dashboard/dashboard/
-aws cloudfront create-invalidation --distribution-id E2DTAKWHWQ7C3J --paths "/*"
-echo new version deployed on s3://tiledesk-dashboard/dashboard/
-echo available on https://support.tiledesk.com/dashboard/index.html
+# aws s3 sync . s3://tiledesk-dashboard/dashboard/
+# aws cloudfront create-invalidation --distribution-id E2DTAKWHWQ7C3J --paths "/*"
+# echo new version deployed on s3://tiledesk-dashboard/dashboard/
+# echo available on https://support.tiledesk.com/dashboard/index.html
 
 
+####### tiledesk V2
+aws s3 sync . s3://tiledesk-console/v1/dashboard/
+echo new version deployed on s3://tiledesk-console/v1/dashboard/
+echo available on https://console.tiledesk.com/v1/dashboard/index.html
