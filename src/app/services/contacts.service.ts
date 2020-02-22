90cec747ed4833ed9873dc67a2da02930af39d00
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-02-22 08:10:06 +0100

    endpoints management via "app config service" and added callback ''get the requests of a contact"

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/map';
 // import { MongodbConfService } from '../utils/mongodb-conf.service';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../core/auth.service';
+import { AppConfigService } from '../services/app-config.service';
 
 @Injectable()
 export class ContactsService {
@@ -19,11 +20,14 @@ export class ContactsService {
   TOKEN: any;
   currentUserID: string;
 
-  BASE_URL = environment.mongoDbConfig.BASE_URL;
+  // BASE_URL = environment.mongoDbConfig.BASE_URL; // replaced with SERVER_BASE_PATH
+  // SERVER_BASE_PATH = environment.SERVER_BASE_URL; // now get from appconfig
+  SERVER_BASE_PATH: string;
 
   constructor(
     http: Http,
-    public auth: AuthService
+    public auth: AuthService,
+    public appConfigService: AppConfigService
 
   ) {
 
@@ -38,6 +42,13 @@ export class ContactsService {
       this.user = user;
       this.checkUser()
     });
+
+    this.getAppConfig();
+  }
+
+  getAppConfig() {
+    this.SERVER_BASE_PATH = this.appConfigService.getConfig().SERVER_BASE_URL;
+    console.log('AppConfigService getAppConfig (CONTACTS-SERV) SERVER_BASE_PATH', this.SERVER_BASE_PATH);
   }
 
   getCurrentProject() {
@@ -68,7 +79,7 @@ export class ContactsService {
     if (querystring === undefined || !querystring) {
       _querystring = ''
     }
-    const url = this.BASE_URL + this.projectId + '/leads?page=' + pagenumber + _querystring;
+    const url = this.SERVER_BASE_PATH + this.projectId + '/leads?page=' + pagenumber + _querystring;
     // use this to test
     // 5bcf51dbc375420015542b5f is the id og the project (in production ) progetto test 23 ott of the user lanzilottonicola74@gmail.com
     // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads?page=' + pagenumber + _querystring;
@@ -92,7 +103,7 @@ export class ContactsService {
     if (querystring === undefined || !querystring) {
       _querystring = ''
     }
-    const url = this.BASE_URL + this.projectId + '/leads/csv?page=' + pagenumber + _querystring;
+    const url = this.SERVER_BASE_PATH + this.projectId + '/leads/csv?page=' + pagenumber + _querystring;
     // use this to test
     // 5bcf51dbc375420015542b5f is the id og the project (in production ) progetto test 23 ott of the user lanzilottonicola74@gmail.com
     // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads?page=' + pagenumber + _querystring;
@@ -114,7 +125,7 @@ export class ContactsService {
 
   // GET LEAD BY ID
   public getLeadById(id: string): Observable<Contact[]> {
-    const url = this.BASE_URL + this.projectId + '/leads/' + id;
+    const url = this.SERVER_BASE_PATH + this.projectId + '/leads/' + id;
 
     /****** use this to test *******/
     // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads/' + id;
@@ -139,7 +150,7 @@ export class ContactsService {
    */
   public updateLead(id: string, fullName: string, _email: string) {
 
-    const url = this.BASE_URL + this.projectId + '/leads/' + id;
+    const url = this.SERVER_BASE_PATH + this.projectId + '/leads/' + id;
 
     /****** use this to test *******/
     // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads/' + id;
@@ -177,7 +188,7 @@ export class ContactsService {
    */
   public deleteLead(id: string) {
 
-    const url = this.BASE_URL + this.projectId + '/leads/' + id;
+    const url = this.SERVER_BASE_PATH + this.projectId + '/leads/' + id;
 
     /****** use this to test *******/
     // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads/' + id;
@@ -197,6 +208,27 @@ export class ContactsService {
       .map((res) => res.json());
 
   }
+
+  public getNodeJsRequestsByRequesterId(requesterid: string, pagenumber: number) {
+    /* *** USED TO TEST IN LOCALHOST (note: this service doen't work in localhost) *** */
+    // const url = 'https://api.tiledesk.com/v1/' + '5ba35f0b9acdd40015d350b6' + '/requests?requester_id=' + requesterid + '&page=' + pagenumber;
+    /* *** USED IN PRODUCTION *** */
+    const url = this.SERVER_BASE_PATH + this.projectId + '/requests?lead=' + requesterid + '&page=' + pagenumber;
+
+    console.log('!!!! CONTACT DETAILS - REQUESTS SERVICE URL ', url);
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    /* *** USED TO TEST IN LOCALHOST (note: this service doesn't work in localhost) *** */
+    // headers.append('Authorization', 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIkX18iOnsic3RyaWN0TW9kZSI6dHJ1ZSwic2VsZWN0ZWQiOnsiZW1haWwiOjEsImZpcnN0bmFtZSI6MSwibGFzdG5hbWUiOjEsInBhc3N3b3JkIjoxLCJlbWFpbHZlcmlmaWVkIjoxLCJpZCI6MX0sImdldHRlcnMiOnt9LCJfaWQiOiI1YWFhOTJmZjRjM2IxMTAwMTRiNDc4Y2IiLCJ3YXNQb3B1bGF0ZWQiOmZhbHNlLCJhY3RpdmVQYXRocyI6eyJwYXRocyI6eyJwYXNzd29yZCI6ImluaXQiLCJlbWFpbCI6ImluaXQiLCJsYXN0bmFtZSI6ImluaXQiLCJmaXJzdG5hbWUiOiJpbml0IiwiX2lkIjoiaW5pdCJ9LCJzdGF0ZXMiOnsiaWdub3JlIjp7fSwiZGVmYXVsdCI6e30sImluaXQiOnsibGFzdG5hbWUiOnRydWUsImZpcnN0bmFtZSI6dHJ1ZSwicGFzc3dvcmQiOnRydWUsImVtYWlsIjp0cnVlLCJfaWQiOnRydWV9LCJtb2RpZnkiOnt9LCJyZXF1aXJlIjp7fX0sInN0YXRlTmFtZXMiOlsicmVxdWlyZSIsIm1vZGlmeSIsImluaXQiLCJkZWZhdWx0IiwiaWdub3JlIl19LCJwYXRoc1RvU2NvcGVzIjp7fSwiZW1pdHRlciI6eyJkb21haW4iOm51bGwsIl9ldmVudHMiOnt9LCJfZXZlbnRzQ291bnQiOjAsIl9tYXhMaXN0ZW5lcnMiOjB9LCIkb3B0aW9ucyI6dHJ1ZX0sImlzTmV3IjpmYWxzZSwiX2RvYyI6eyJsYXN0bmFtZSI6IkxhbnppbG90dG8iLCJmaXJzdG5hbWUiOiJOaWNvbGEgNzQiLCJwYXNzd29yZCI6IiQyYSQxMCRwVWdocTVJclgxMzhTOXBEY1pkbG1lcnNjVTdVOXJiNlFKaVliMXlEckljOHJDMFh6c2hUcSIsImVtYWlsIjoibGFuemlsb3R0b25pY29sYTc0QGdtYWlsLmNvbSIsIl9pZCI6IjVhYWE5MmZmNGMzYjExMDAxNGI0NzhjYiJ9LCIkaW5pdCI6dHJ1ZSwiaWF0IjoxNTM4MTIzNTcyfQ.CYnxkLbg5XWk2JWAxQg1QNGDpNgNbZAzs5PEQpLCCnI');
+    /* *** USED IN PRODUCTION *** */
+    headers.append('Authorization', this.TOKEN);
+
+    return this.http
+      .get(url, { headers })
+      .map((response) => response.json());
+  }
+  
 
   /**
    * CREATE (POST)
