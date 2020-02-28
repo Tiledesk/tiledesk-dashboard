7ebf99e24f5721b53baea7b88655b5b0f495ae3e
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-02-28 16:02:52 +0100

    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,41 +1,22 @@
 
-const serverUrl = 'https://api.tiledesk.com/v1/';
 export const environment = {
     production: true,
-    t2y12PruGU9wUtEGzBJfolMIgK: 'PAY:F-ANA:F-ACT:F-TRI:F-GRO:F-DEP:F-OPH:F-MTL:F-DGF:F',
+    t2y12PruGU9wUtEGzBJfolMIgK: 'PAY:F-ANA:F-ACT:F-TRI:F-GRO:F-DEP:F-OPH:F-MTL:F-DGF:F-NAT:F-CAR:F',
     VERSION: require('../../package.json').version,
     widgetUrl: 'https://widget.tiledesk.com/v3/launch.js',
+    botcredendialsURL: 'CHANGE-IT',
     remoteConfig: false, // for performance don't load settings from remote
+    SERVER_BASE_URL: 'https://api.tiledesk.com/v1/',
+    CHAT_BASE_URL: 'https://support.tiledesk.com/chat/',
+    testsiteBaseUrl: 'https://widget.tiledesk.com/v3/assets/twp/index.html',
+    wsUrl: 'wss://tiledesk-server-pre.herokuapp.com/',
     firebase: {
-        apiKey: 'CHANGEIT',
-        authDomain: 'CHANGEIT',
-        databaseURL: 'CHANGEIT',
-        projectId: 'CHANGEIT',
-        storageBucket: 'CHANGEIT',
-        messagingSenderId: 'CHANGEIT',
-        chat21ApiUrl: 'CHANGEIT'
+        apiKey: "CHANGE-IT",
+        authDomain: "CHANGE-IT",
+        databaseURL: "CHANGE-IT",
+        projectId: "CHANGE-IT",
+        storageBucket: "CHANGE-IT",
+        messagingSenderId: "CHANGE-IT",
     },
-    mongoDbConfig: {
-        BASE_URL: `${serverUrl}`,
-        PROJECTS_BASE_URL: `${serverUrl}projects/`,
-        SIGNUP_BASE_URL: `${serverUrl}auth/signup`,
-        SIGNIN_BASE_URL: `${serverUrl}auth/signin`,
-        FIREBASE_SIGNIN_BASE_URL: `${serverUrl}firebase/auth/signin`,
-        VERIFY_EMAIL_BASE_URL: `${serverUrl}auth/verifyemail/`,
-        REQUEST_RESET_PSW: `${serverUrl}auth/requestresetpsw`,
-        RESET_PSW: `${serverUrl}auth/resetpsw/`,
-        CHECK_PSW_RESET_KEY: `${serverUrl}auth/checkpswresetkey/`,
-        UPDATE_USER_LASTNAME_FIRSTNAME: `${serverUrl}users/updateuser/`,
-        CHANGE_PSW: `${serverUrl}users/changepsw/`,
-        RESEND_VERIFY_EMAIL: `${serverUrl}users/resendverifyemail/`,
-    },
-    chat: {
-        CHAT_BASE_URL: 'https://support.tiledesk.com/chat/',
-    },
-    testsite: {
-        testsiteBaseUrl: 'https://widget.tiledesk.com/v3/assets/twp/index.html'
-    },
-    websocket: {
-        wsUrl: 'wss://tiledesk-server-pre.herokuapp.com?token='
-    }
 };
+
