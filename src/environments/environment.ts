e4da51c63a7c88e42e541c6882b6d031e599b18e
Andrea Leo <andrealeo83@users.noreply.github.com>
2020-02-28 09:54:54 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
     t2y12PruGU9wUtEGzBJfolMIgK: 'PAY:F-ANA:F-ACT:F-TRI:F-GRO:F-DEP:F-OPH:F-MTL:F-DGF:F-NAT:F',
     widgetUrl: 'http://localhost:4200/launch.js',
     botcredendialsURL: 'CHANGEIT',
-    SERVER_BASE_URL: '/api/',
-    CHAT_BASE_URL: '/chat/',
+    SERVER_BASE_URL: 'http://localhost:3000/',
+    CHAT_BASE_URL: 'http://localhost:8080/',
     testsiteBaseUrl: 'http://localhost:4200/assets/test_widget_page/index.html',
     wsUrl: 'ws://' + window.location.hostname + '/ws/?token=',
     firebase: {
