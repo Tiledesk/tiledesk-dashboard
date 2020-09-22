741ed5338f0e66ba07b816fb7f26dbdbcd2729fc
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-09-22 17:38:05 +0200

    scroolbar directive

---
+++ src/app/perfect-scrollbar-td.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PerfectScrollbarTdDirective } from './perfect-scrollbar-td.directive';
+
+describe('PerfectScrollbarTdDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PerfectScrollbarTdDirective();
+    expect(directive).toBeTruthy();
+  });
+});
