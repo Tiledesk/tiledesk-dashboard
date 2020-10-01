35678f4f793dcc08a57b4fc77a65fd3bf4abc33c
Andrea Leo <andrealeo83@users.noreply.github.com>
2020-10-01 10:44:45 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ export const environment = {
     production: true,
     t2y12PruGU9wUtEGzBJfolMIgK: 'PAY:F-ANA:F-ACT:F-TRI:F-GRO:F-DEP:F-OPH:F-MTL:F-DGF:F-NAT:F-CAR:F-V1L:F-PSA:F',
     VERSION: require('../../package.json').version,
-    widgetUrl: 'https://widget.tiledesk.com/v3/launch.js',
+    widgetUrl: 'https://widget.tiledesk.com/v4/launch.js',
     botcredendialsURL: 'CHANGE-IT',
-    remoteConfig: false, // for performance don't load settings from remote
-    SERVER_BASE_URL: 'https://api.tiledesk.com/v1/',
-    CHAT_BASE_URL: 'https://support.tiledesk.com/chat/',
-    testsiteBaseUrl: 'https://widget.tiledesk.com/v3/assets/twp/index.html',
-    wsUrl: 'wss://tiledesk-server-pre.herokuapp.com/',
+    remoteConfig: true, 
+    SERVER_BASE_URL: 'https://api.tiledesk.com/v2/',
+    CHAT_BASE_URL: 'https://console.tiledesk.com/v2/chat/',
+    testsiteBaseUrl: 'https://widget.tiledesk.com/v4/assets/twp/index.html',
+    wsUrl: 'wss://eu.rtm.tiledesk.com/v2/ws/',
     firebase: {
         apiKey: "CHANGE-IT",
         authDomain: "CHANGE-IT",
@@ -19,4 +19,3 @@ export const environment = {
         messagingSenderId: "CHANGE-IT",
     },
 };
-
