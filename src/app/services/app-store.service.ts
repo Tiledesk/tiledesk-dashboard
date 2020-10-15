7d3b2e2a439cd6c3e3138314f92e3c4f2002a224
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-10-15 13:08:26 +0200

    Fixes in the app service the call to get the apps

--- src/app/services/app-store.service.ts
+++ src/app/services/app-store.service.ts
@@ -1,36 +1,60 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
-
+import { AuthService } from '../core/auth.service';
 @Injectable()
 
 export class AppStoreService {
 
   http: Http;
+  TOKEN: string;
+  APPS_URL = "https://tiledesk-apps.herokuapp.com/api/apps?sort=score";
 
   constructor(
-    http: Http
-
+    http: Http,
+    public auth: AuthService,
   ) {
     this.http = http;
 
+    this.getToken();
+  }
+
+  getToken() {
+    this.auth.user_bs.subscribe((user) => {
+      if (user) {
+        this.TOKEN = user.token
+      }
+    });
   }
 
 
 
-  getApps() {
-    // const url = '/assets/mock-data/apps.json';
-    // const url =  "https://lightgreymountainousscreenscraper--five-nine.repl.co/apps"
-    const url =  "https://tiledesk-apps.herokuapp.com/api/apps?sort=score"
+  // _getApps() {
+  //   // const url = '/assets/mock-data/apps.json';
+  //   // const url =  "https://lightgreymountainousscreenscraper--five-nine.repl.co/apps"
+  //   const url = "https://tiledesk-apps.herokuapp.com/api/apps?sort=score"
+  //   const headers = new Headers();
+  //   headers.append('Content-Type', 'application/json');
+
+  //   return this.http
+  //     .get(url, { headers })
+  //     .pipe(map((response: any) => {
+  //       return response.json();
+  //     }
+  //     ));
+  // }
+
+
+  public getApps() {
+    let url = this.APPS_URL;
+  
+
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-
+    headers.append('Authorization', this.TOKEN);
     return this.http
       .get(url, { headers })
-      .pipe(map((response: any) => {
-        return response.json();
-      }
-      ));
+      .map((response) => response.json());
   }
 
 }
