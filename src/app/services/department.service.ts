1dd3ba5fb0568a14f523f40e1c5634fa231cdf5b
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-10-27 11:14:27 +0100

    Fixes the departments status updating service

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -336,7 +336,7 @@ export class DepartmentService {
     console.log('UPDATE DEPT STATUS - REQUEST BODY ', body);
 
     return this.http
-      .put(url, JSON.stringify(body), options)
+      .patch(url, JSON.stringify(body), options)
       .map((res) => res.json());
   }
 
