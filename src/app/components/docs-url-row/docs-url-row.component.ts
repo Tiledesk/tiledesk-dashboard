e49dc65f8b6f9816654e1b08ffee3137afcab215
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-03-03 20:14:45 +0100

    Improved component

--- src/app/components/docs-url-row/docs-url-row.component.ts
+++ src/app/components/docs-url-row/docs-url-row.component.ts
@@ -9,6 +9,8 @@ export class DocsUrlRowComponent implements OnInit {
   @Input() doctitle: string;
   @Input() docurl: string;
   @Input() customtext: boolean;
+  @Input() text_to_display: string;
+  
   translateparam: any; 
   constructor() { }
 
@@ -16,7 +18,8 @@ export class DocsUrlRowComponent implements OnInit {
     this.translateparam = { helpdoc: this.doctitle };
     console.log('Docs_Url_Row Component doctitle ', this.doctitle);
     console.log('Docs_Url_Row Component docurl ', this.docurl)  
-    console.log('Docs_Url_Row Component customtext ', this.docurl)  
+    console.log('Docs_Url_Row Component customtext ', this.customtext)  
+    console.log('Docs_Url_Row Component text_to_display ', this.text_to_display) 
   }
 
 }
