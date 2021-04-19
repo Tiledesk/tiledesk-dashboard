ee9420c4e249d262212542305054c9be8a102ef1
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-04-19 09:29:36 +0200

    Improves the loading spinner on the apps installation page

--- src/app/app-store/app-store-install/app-store-install.component.ts
+++ src/app/app-store/app-store-install/app-store-install.component.ts
@@ -39,7 +39,7 @@ export class AppStoreInstallComponent implements OnInit {
   ngOnInit() {
 
     this.onInitframeHeight();
-    
+
   }
 
 
@@ -65,7 +65,7 @@ export class AppStoreInstallComponent implements OnInit {
             this.URL = this.sanitizer.bypassSecurityTrustResourceUrl(parsed_json.installActionURL + '?project_id=' + params.projectid + '&token=' + this.TOKEN);
             console.log("APP-STORE-INSTALL - URL IFRAME: ", this.URL)
             this.getIframeHaLoaded()
-            
+
           } else {
             console.log("APP-STORE-INSTALL - GET USER TOKEN: FAILED");
             this.showSpinner = false;
@@ -82,31 +82,45 @@ export class AppStoreInstallComponent implements OnInit {
     })
   }
 
-
   getIframeHaLoaded() {
     var self = this;
     var iframe = document.getElementById('i_frame') as HTMLIFrameElement;;
     console.log('APP-STORE-INSTALL GET iframe ', iframe)
-    var iframeDoc = iframe.contentDocument;
-    console.log('APP-STORE-INSTALL GET iframeDoc ', iframeDoc)
+    iframe.addEventListener("load", function () {
+      console.log("APP-STORE-INSTALL GET - Finish");
+      let spinnerElem = <HTMLElement>document.querySelector('.stretchspinner_in_app_install')
+      // let spinnerElem = document.getElementsByClassName("stretchspinner_in_app_install")  as HTMLCollectionOf<HTMLElement>;
+      console.log('APP-STORE-INSTALL GET iframeDoc readyState spinnerElem', spinnerElem)
+      spinnerElem.classList.add("hide-stretchspinner")
 
-    // Check if loading is complete
-    if (iframeDoc.readyState == 'complete') {
-      console.log('APP-STORE-INSTALL GET iframeDoc readyState', iframeDoc.readyState)
-      console.log('APP-STORE-INSTALL GET iframeDoc readyState  iframeDoc',  iframeDoc)
-      // this.ngZone.run( () => {
-        self.showSpinner = false;
-         let spinnerElem =  <HTMLElement>document.querySelector('.stretchspinner_in_app_install')
-        // let spinnerElem = document.getElementsByClassName("stretchspinner_in_app_install")  as HTMLCollectionOf<HTMLElement>;
-        console.log('APP-STORE-INSTALL GET iframeDoc readyState spinnerElem',  spinnerElem)
-        spinnerElem.classList.add("hide-stretchspinner")
-      // });
-      console.log('APP-STORE-INSTALL GET iframeDoc readyState   this.showSpinner',   self.showSpinner)
-    } else {
-         // If we are here, it is not loaded. Set things up so we check   the status again in 100 milliseconds
-    window.setTimeout(this.getIframeHaLoaded, 100)
-    }
+    });
   }
+
+
+  // _getIframeHaLoaded() {
+  //   var self = this;
+  //   var iframe = document.getElementById('i_frame') as HTMLIFrameElement;;
+  //   console.log('APP-STORE-INSTALL GET iframe ', iframe)
+  //   var iframeDoc = iframe.contentDocument;
+  //   console.log('APP-STORE-INSTALL GET iframeDoc ', iframeDoc)
+
+  //   // Check if loading is complete
+  //   if (iframeDoc.readyState == 'complete') {
+  //     console.log('APP-STORE-INSTALL GET iframeDoc readyState', iframeDoc.readyState)
+  //     console.log('APP-STORE-INSTALL GET iframeDoc readyState  iframeDoc', iframeDoc)
+  //     // this.ngZone.run( () => {
+  //     self.showSpinner = false;
+  //     let spinnerElem = <HTMLElement>document.querySelector('.stretchspinner_in_app_install')
+  //     // let spinnerElem = document.getElementsByClassName("stretchspinner_in_app_install")  as HTMLCollectionOf<HTMLElement>;
+  //     console.log('APP-STORE-INSTALL GET iframeDoc readyState spinnerElem', spinnerElem)
+  //     spinnerElem.classList.add("hide-stretchspinner")
+  //     // });
+  //     console.log('APP-STORE-INSTALL GET iframeDoc readyState   this.showSpinner', self.showSpinner)
+  //   } else {
+  //     // If we are here, it is not loaded. Set things up so we check   the status again in 100 milliseconds
+  //     window.setTimeout(this._getIframeHaLoaded, 100)
+  //   }
+  // }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
@@ -132,6 +146,7 @@ export class AppStoreInstallComponent implements OnInit {
   }
 
   goBack() {
+    console.log("APP-STORE-INSTALL - goBack");
     this.location.back();
   }
 
