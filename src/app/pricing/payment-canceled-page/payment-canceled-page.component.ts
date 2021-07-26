5e47a992ed901f3ec33dd8152ee82c02d5d9b809
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-07-26 19:45:22 +0200

    Adds the "Payment Canceled" page displayed when a subscription error occurs

--- src/app/pricing/payment-canceled-page/payment-canceled-page.component.ts
+++ src/app/pricing/payment-canceled-page/payment-canceled-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/auth.service';
+import { BrandService } from '../../services/brand.service';
+import { LoggerService } from '../../services/logger/logger.service';
 
 @Component({
   selector: 'appdashboard-payment-canceled-page',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentCanceledPageComponent implements OnInit {
 
-  constructor() { }
+  contact_us_email: string;
+  id_project: string;
+
+  constructor(  
+     private router: Router,
+    private auth: AuthService,
+    public brandService: BrandService,
+    private logger: LoggerService) { 
+      const brand = brandService.getBrand();
+      this.contact_us_email = brand['contact_us_email'];
+    }
 
   ngOnInit() {
+    this.getCurrentProject();
   }
+
+  getCurrentProject() {
+    this.auth.project_bs.subscribe((project) => {
+      if (project) {
+        this.id_project = project._id
+        this.logger.log('[PRICING - PAYMENT-CANCELED] - CurrentProject ID ', this.id_project)
+      }
+    });
+  }
+
+
+  goToHome() {
+    this.router.navigate(['/project/' + this.id_project + '/home']);
+  }
+
 
 }
