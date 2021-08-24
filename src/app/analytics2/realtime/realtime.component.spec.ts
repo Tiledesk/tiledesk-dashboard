41d3beb08f82544a9b63358a9c321647b160dc08
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-08-24 08:50:51 +0200

    Adds unit tests

--- src/app/analytics2/realtime/realtime.component.spec.ts
+++ src/app/analytics2/realtime/realtime.component.spec.ts
@@ -2,13 +2,55 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RealtimeComponent } from './realtime.component';
 
+import { TranslateModule } from '@ngx-translate/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MomentModule } from 'angular2-moment';
+import { UsersService } from './../../services/users.service';
+import { FaqKbService } from './../../services/faq-kb.service';
+import { BotLocalDbService } from './../../services/bot-local-db.service';
+import { HttpModule } from '@angular/http';
+
+import { AuthService } from './../../core/auth.service';
+import { NotifyService } from './../../core/notify.service';  // Error: StaticInjectorError(DynamicTestModule)[AuthService -> NotifyService]:
+import { LocalDbService } from './../../services/users-local-db.service'; // Error: StaticInjectorError(DynamicTestModule)[AuthService -> LocalDbService]: 
+import { WebSocketJs } from "./../../services/websocket/websocket-js"; // Error: StaticInjectorError(DynamicTestModule)[AuthService -> WebSocketJs]: 
+import { RouterTestingModule } from '@angular/router/testing'; // Error: StaticInjectorError(DynamicTestModule)[AuthService -> Router]: 
+import { LoggerService } from './../../services/logger/logger.service';
+import { AppConfigService } from './../../services/app-config.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DepartmentService } from './../../services/department.service';
+import { WsRequestsService } from './../../services/websocket/ws-requests.service';
+import { AnalyticsService } from './../../services/analytics.service';
+
 describe('RealtimeComponent', () => {
   let component: RealtimeComponent;
   let fixture: ComponentFixture<RealtimeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RealtimeComponent ]
+      declarations: [ RealtimeComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        TranslateModule.forRoot(),
+        MomentModule,
+        HttpModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        UsersService,
+        FaqKbService,
+        BotLocalDbService,
+        AuthService,
+        NotifyService,
+        LocalDbService,
+        WebSocketJs,
+        LoggerService,
+        AppConfigService,
+        DepartmentService,
+        WsRequestsService,
+        AnalyticsService
+       ],
     })
     .compileComponents();
   }));
