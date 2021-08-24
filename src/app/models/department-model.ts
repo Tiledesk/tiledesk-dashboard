06dd5e7a3c5413e8996edcc3830a44ab2367d353
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-08-24 09:10:07 +0200

    Updates the model

--- src/app/models/department-model.ts
+++ src/app/models/department-model.ts
@@ -1,21 +1,24 @@
 export interface Department {
-    _id: string;
-    updatedAt: any;
-    createdAt: any;
-    name: string;
-    id_project: string;
-    createdBy: string;
-    id_bot: string;
-    bot_only: boolean;
-    id_group: string;
-    hasGroupName: string;
-    routing: string;
+    _id?: string;
+    id?: string;
+    updatedAt?: any;
+    createdAt?: any;
+    name?: string;
+    id_project?: string;
+    createdBy?: string;
+    id_bot?: string;
+    bot_only?: boolean;
+    id_group?: string;
+    routing?: string;
     description?: string;
     default?: boolean;
+    status?: number;
+    tags?: any;
     __v: any;
     // custom
     groupHasBeenTrashed?: boolean;
     botHasBeenTrashed?: boolean;
     hasDeptName?: string;
-    status?: number;
+    hasBot?: boolean;
+    hasGroupName?: string;
 }
