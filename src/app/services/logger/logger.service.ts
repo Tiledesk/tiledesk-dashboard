35ace3dc55cc6da72d892168d09b8d2cc6057cea
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-08-24 19:20:43 +0200

    Modifies the "logger service" to accept only values of string type from the "loggingLevel" environments property

--- src/app/services/logger/logger.service.ts
+++ src/app/services/logger/logger.service.ts
@@ -7,6 +7,12 @@ import { LogLevel } from '../../utils/util';
 export class LoggerService {
   // private logLevel: number = LogLevel.Debug
   private logLevel: number;
+
+  // Error = 0,
+  // Warn = 1,
+  // Info = 2,
+  // Debug = 3
+
   constructor(
     public appConfigService: AppConfigService
   ) {
@@ -26,80 +32,93 @@ export class LoggerService {
 
     const config = this.appConfigService.getConfig();
     // console.log("[LoggerService] current HERE YES config ", config)
-    // console.log("[LoggerService] current HERE YES config loggingLevel", config.loggingLevel)
+    console.log("[LoggerService] current HERE YES config loggingLevel", config.loggingLevel)
 
-    if (config.loggingLevel !== undefined) {
+    if (config.loggingLevel !== undefined && config.loggingLevel.length > 0) {
+
+
+
       if (this.appConfigService.getConfig().loggingLevel) {
-        this.logLevel = this.appConfigService.getConfig().loggingLevel
+        if (config.loggingLevel === "Error") {
+          this.logLevel = 0
+        } else if (config.loggingLevel === "Warn") {
+          this.logLevel = 1
+        } else if (config.loggingLevel === "Info") {
+          this.logLevel = 2
+        } else if (config.loggingLevel === "Debug") {
+          this.logLevel = 3
+        }
+
+        // this.logLevel = this.appConfigService.getConfig().loggingLevel
         // cons3ole.log("[LoggerService] current logLevel", this.logLevel)
         // this.setLoglevel(this.logLevel)
-      } else {
-        if (this.appConfigService.getConfig().loggingLevel === 0) {
-          // console.log("[LoggerService] current HERE YES else 2 config loggingLevel", config.loggingLevel)
-          this.logLevel = this.appConfigService.getConfig().loggingLevel
+        // } else {
+        //   if (this.appConfigService.getConfig().loggingLevel === 0) {
+        //     // console.log("[LoggerService] current HERE YES else 2 config loggingLevel", config.loggingLevel)
+        //     this.logLevel = this.appConfigService.getConfig().loggingLevel
+        //   }
         }
+      } else if (config.loggingLevel === undefined || config.loggingLevel.length === 0) {
+        console.log("[LoggerService] IS UNDEFINED OR EMPTY ")
+        this.logLevel = 3 // debug
       }
-    } else {
-      // console.log("[LoggerService] loggingLevel is undefined", config.loggingLevel)
-      this.logLevel = 3 // debug
+
     }
 
-  }
 
-
-  error(...message: any[]) {
-    if (this.logLevel >= LogLevel.Error) {
-      console.error(message)
+    error(...message: any[]) {
+      if (this.logLevel >= LogLevel.Error) {
+        console.error(message)
+      }
     }
-  }
 
-  warn(...message: any[]) {
-    if (this.logLevel >= LogLevel.Warn) {
-      console.warn(message)
+    warn(...message: any[]) {
+      if (this.logLevel >= LogLevel.Warn) {
+        console.warn(message)
+      }
     }
-  }
 
-  info(...message: any[]) {
-    if (this.logLevel >= LogLevel.Info) {
-      console.info(message)
+    info(...message: any[]) {
+      if (this.logLevel >= LogLevel.Info) {
+        console.info(message)
+      }
     }
-  }
 
-  debug(...message: any[]) {
-    if (this.logLevel >= LogLevel.Debug) {
-      console.debug(message)
+    debug(...message: any[]) {
+      if (this.logLevel >= LogLevel.Debug) {
+        console.debug(message)
+      }
     }
-  }
 
-  log(...message: any[]) {
-    if (this.logLevel >= LogLevel.Debug) {
-      console.log(message)
+    log(...message: any[]) {
+      if (this.logLevel >= LogLevel.Debug) {
+        console.log(message)
+      }
     }
+
+    // debug(...args: any[]) {
+    //   if (this.logLevel == this.LEVEL_DEBUG) {
+    //     console.debug.apply(console, args)
+    //   }
+    // }
+
+    // log(...args: any[]) {
+    //   if (this.logLevel == this.LEVEL_DEBUG) {
+    //     console.log.apply(console, args)
+    //   }
+    // }
+
+    // info(...args: any[]) {
+    //   if (this.logLevel <= this.LEVEL_INFO) {
+    //     console.info.apply(console, args)
+    //   }
+    // }
+
+    // error(...args: any[]) {
+    //   // if (this.logLevel <= LEVEL_ERROR) {
+    //   console.error.apply(console, args)
+    //   // }
+    // }
+
+
   }
-
-  // debug(...args: any[]) {
-  //   if (this.logLevel == this.LEVEL_DEBUG) {
-  //     console.debug.apply(console, args)
-  //   }
-  // }
-
-  // log(...args: any[]) {
-  //   if (this.logLevel == this.LEVEL_DEBUG) {
-  //     console.log.apply(console, args)
-  //   }
-  // }
-
-  // info(...args: any[]) {
-  //   if (this.logLevel <= this.LEVEL_INFO) {
-  //     console.info.apply(console, args)
-  //   }
-  // }
-
-  // error(...args: any[]) {
-  //   // if (this.logLevel <= LEVEL_ERROR) {
-  //   console.error.apply(console, args)
-  //   // }
-  // }
-
-
-}
