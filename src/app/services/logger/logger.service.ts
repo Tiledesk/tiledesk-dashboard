0ccb78b12b2a37927b82dff6d1113f0dcb889453
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-09-02 19:17:40 +0200

    Enhances the log service to detect invalid values assigned to the "logLevel" environment variable

--- src/app/services/logger/logger.service.ts
+++ src/app/services/logger/logger.service.ts
@@ -39,22 +39,32 @@ export class LoggerService {
     if (config.logLevel !== undefined) {
 
       if (typeof this.appConfigService.getConfig().logLevel === 'string') {
+        
         if (config.logLevel.length > 0) {
           if (config.logLevel.toUpperCase() === "ERROR") {
+            console.info('%c ### DSHBRD [LOGGER-SERV] Log Level: ERROR ', 'color: #1a73e8' );
             this.logLevel = 0
           } else if (config.logLevel.toUpperCase() === "WARN") {
+            console.info('%c ### DSHBRD [LOGGER-SERV] Log Level: WARN ', 'color: #1a73e8' );
             this.logLevel = 1
           } else if (config.logLevel.toUpperCase() === "INFO") {
+            console.info('%c ### DSHBRD [LOGGER-SERV] Log Level: INFO ', 'color: #1a73e8' );
             this.logLevel = 2
           } else if (config.logLevel.toUpperCase() === "DEBUG") {
+            console.info('%c ### DSHBRD [LOGGER-SERV] Log Level: DEBUG ', 'color: #1a73e8' );
             this.logLevel = 3
+          } else if (config.logLevel.toUpperCase() !== "ERROR" || config.logLevel.toUpperCase() === "WARN" || config.logLevel.toUpperCase() !== "INFO" || config.logLevel.toUpperCase() !== "DEBUG") {
+            console.error('logLevel has no valid value! Will be used the Debug log level. See the tiledesk-dashboard README.md available on https://github.com/Tiledesk/tiledesk-dashboard#dashboard-configjson ')
+            this.logLevel = 3 // debug
           }
         } else {
           console.error('logLevel has an empty value! Will be used the Debug log level ')
           this.logLevel = 3 // debug
         }
       } else {
-        console.error('logLevel is not a string. See the tiledesk-dashboard README.md available on https://github.com/Tiledesk/tiledesk-dashboard#dashboard-configjson')
+        console.error('logLevel is not a string. Will be used the Debug log level. See the tiledesk-dashboard README.md available on https://github.com/Tiledesk/tiledesk-dashboard#dashboard-configjson')
+
+        this.logLevel = 3 // debug
       }
       // || config.loggingLevel.length === 0
     } else if (config.loggingLevel === undefined) {
