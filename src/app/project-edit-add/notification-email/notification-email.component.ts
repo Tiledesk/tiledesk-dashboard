f0188690bc2ca5fed6f0eb292fac94a49ba4d876
nicola74 <nicola.lanzilotto@frontiere21.it>
2022-01-06 20:06:04 +0100

    Adds the ability to customize notification email templates (improvements)

--- src/app/project-edit-add/notification-email/notification-email.component.ts
+++ src/app/project-edit-add/notification-email/notification-email.component.ts
@@ -1,6 +1,10 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, SecurityContext } from '@angular/core';
 import { Location } from '@angular/common';
 import { ProjectService } from '../../services/project.service';
+import { DomSanitizer } from '@angular/platform-browser'
+import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'app/core/auth.service';
+
 declare const $: any;
 @Component({
   selector: 'appdashboard-notification-email',
@@ -9,243 +13,140 @@ declare const $: any;
 })
 export class NotificationEmailComponent implements OnInit, AfterViewInit {
 
-  assignedRequestTemplate: string;
-
+  emailTemplate: any;
+  iframe: any;
+  iframeContent: any;
+  textToSearch: string;
+  EMAIL_TEMPLATE_NAME = ["assignedRequest.html", "assignedEmailMessage.html", "pooledRequest.html"]
+  iframePlaceholderText: string;
+  active_template: string = "assignedRequest.html"
+  iFrame_placeholder: string 
   constructor(
     public location: Location,
-    public projectService: ProjectService
-
+    public projectService: ProjectService,
+    private sanitizer: DomSanitizer,
+    private translate: TranslateService,
+    private auth: AuthService,
   ) { }
 
   ngOnInit() {
     this.getAssignedRequestTemplate();
-
+    console.log('[NOTIFICATION-EMAIL] - GET EMAIL TEMPALTE - ACTIVE TEMPLATE ', this.active_template);
+    this.auth.checkRoleForCurrentProjectPermissionOnlyToOwner
   }
 
+ 
 
-
-  ngAfterViewInit(): void {
-
-
+  ngAfterViewInit(){
+    this.disableIframeLinks();
+    this.getTranslations();
   }
+
+  getTranslations() {
+    this.translate.get('ProjectEditPage.SeeThePreviewOfTheEmail')
+      .subscribe((text: string) => {
+        console.log('[NOTIFICATION-EMAIL] - GET TRANSLATION - text ', text);
+        this.iframePlaceholderText = text;
+        this.iFrame_placeholder = '<p id="iframe-placeholder" style="text-align:center; line-height: 484px;">'+ this.iframePlaceholderText + '</p>';
+        (<HTMLIFrameElement>document.getElementById("iframe-email-template-preview")).srcdoc = this.iFrame_placeholder;
+      });
+    }
+
+  disableIframeLinks() {
+    $("iframe").on('load', function () {
+      $("iframe").contents().find("a").each(function (index) {
+        $(this).on("click", function (event) {
+          event.preventDefault();
+          event.stopPropagation();
+        });
+      });
+    });
+  }
+
 
   goBack() {
     this.location.back();
   }
+
+  selectedTemplate(tmplt) {
+    console.log('[NOTIFICATION-EMAIL] - GET EMAIL TEMPALTE - SELECTED TEMPALTE ', tmplt)
+    this.active_template = tmplt
+    console.log('[NOTIFICATION-EMAIL] - GET EMAIL TEMPALTE - ACTIVE TEMPLATE ', this.active_template)
+    this.getAssignedRequestTemplate();
+    (<HTMLIFrameElement>document.getElementById("iframe-email-template-preview")).srcdoc = this.iFrame_placeholder;
+  }
+
   getAssignedRequestTemplate() {
-    this.projectService.getEmailTemplate('assignedRequest.html').subscribe((res: any) => {
-      console.log('GET ASSIGNED REQUESTS TEMPALTE res ', res)
+    this.projectService.getEmailTemplate(this.active_template).subscribe((res: any) => {
+      console.log('[NOTIFICATION-EMAIL] - GET EMAIL TEMPALTE res ', res)
       if (res) {
-        this.assignedRequestTemplate = res.template
+        this.emailTemplate = res.template
+        this.iframeContent = res.template
       }
     }, (error) => {
-     console.log('GET ASSIGNED REQUESTS TEMPALTE ', error);
+      console.log('[NOTIFICATION-EMAIL] - GET EMAIL TEMPALTE error ', error);
     }, () => {
-      console.log('GET ASSIGNED REQUESTS TEMPALTE * COMPLETE *');
+      console.log('[NOTIFICATION-EMAIL] - GET EMAIL TEMPALTE  * COMPLETE *');
 
     });
   }
 
 
-  _getAssignedRequestTemplate() {
-    this.assignedRequestTemplate = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-    
-      <head>
-        <meta name="viewport" content="width=device-width" />
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>Request transcript</title>
-    
-        <style type="text/css">
-          img {
-            max-width: 100%;
-            margin-left:16px;
-            margin-bottom:16px;
-            text-align:center !important;
-          }
-          body {
-            -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em;
-          }
-          body {
-            background-color: #f6f6f6;
-          }
-    
-          @media only screen and (max-width: 640px) {
-            body {
-              padding: 0 !important;
-            }
-            h1 {
-              font-weight: 800 !important; margin: 20px 0 5px !important;
-              text-align:center !important;
-            }
-            h2 {
-              font-weight: 800 !important; margin: 20px 0 5px !important;
-            }
-            h3 {
-              font-weight: 800 !important; margin: 20px 0 5px !important;
-            }
-            h4 {
-              font-weight: 800 !important; margin: 20px 0 5px !important;
-            }
-            h1 {
-              font-size: 22px !important;
-            }
-            h2 {
-              font-size: 18px !important;
-            }
-            h3 {
-              font-size: 16px !important;
-            }
-            .container {
-              padding: 0 !important; width: 100% !important;
-            }
-            .content {
-              padding: 0 !important;
-            }
-            .content-wrap {
-              padding: 10px !important;
-            }
-            .invoice {
-              width: 100% !important;
-            }
-          }
-        </style>
-      </head>
-    
-      <body itemscope itemtype="http://schema.org/EmailMessage" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6">
-    
-        <table class="body-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6">
-          <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-            <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
-            <td class="container" width="600" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto;" valign="top">
-              <div class="content" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
-                <table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background-color: #fff; margin: 0; border: 1px solid #e9e9e9;" bgcolor="#fff">
-                
-                <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-  
-                <div style="text-align:center">
-                  <a href="http://www.tiledesk.com" style="color:#2daae1;font-weight:bold;text-decoration:none;word-break:break-word" target="_blank">
-                    <img src="http://tiledesk.com/wp-content/uploads/2018/03/tiledesk-logo.png" style="width:50%;outline:none;text-decoration:none;border:none;min-height:36px" class="CToWUd">
-                  </a>
-                </div>
-                </tr>
-  
-                   
-                  
-                  <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                    <td class="content-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
-                      <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-    
-  
-  
-                            <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-  
-                              <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                              <strong style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">Hi,</strong>
-                              <br><br> here's the transcript of your conversation.
-  
-                              </td>
-                            </tr>
-  
-  
-                            <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                            <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                              Chat started on <span style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"></span>
-                            </td>
-                            </tr>
-  
-                        
-                          <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                              <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                                <strong style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                        
-                            </strong>
-                              </td>
-                            </tr>
-  
-                              
-                            <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                              <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                              </td>
-                            </tr>
-                      </table>
-                      </td>
-                      </tr>
-  
-  
-        
-                      <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                      <td class="content-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
-                        
-      
-                      <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-  
-                          <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                            <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                              Department : <span style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"></span>
-                            </td>
-                          </tr>
-  
-                          <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                            <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                              Source : <span style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"></span>
-                            </td>
-                          </tr>
-  
-                          <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                            <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                              User Agent : <span style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"></span>
-                            </td>
-                          </tr>
-  
-                          <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                          <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                            Language : <span style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"></span>
-                          </td>
-                        </tr>
 
-                      </table>
-                    </td>
-                  </tr>
-                </table>
-                <div class="footer" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;">
-                  <table width="100%" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                    <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                      <td class="aligncenter content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; color: #999; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">
-                        <span><a href="http://www.tiledesk.com" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;" > Tiledesk.com </a></span>
-                       <!-- <br><span>Powered by <a href="http://www.frontiere21.com" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;">Frontiere21</a></span> -->
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-              </div>
-            </td>
-            <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
-          </tr>
-        </table>
-      </body>
-    </html>
-    `
-    setTimeout(() => {
-      this.refresh();
-    }, 500);
+  seePreview() {
+    (<HTMLIFrameElement>document.getElementById("iframe-email-template-preview")).srcdoc = this.emailTemplate;
+    const see_preview_btn = <HTMLElement>document.querySelector('.see_preview_btn');
+    see_preview_btn.blur();
+  
   }
+
+  ftxt() {
+    console.log('[NOTIFICATION-EMAIL] FIND TEXT ON TEXAREA textToSearch', this.textToSearch)
+    if (this.emailTemplate.includes(this.textToSearch)) {
+      console.log('[NOTIFICATION-EMAIL] FIND TEXT ON TEXAREA TEXT FOUND')
+      const regex = new RegExp(this.textToSearch, 'gi');
+      // let text = this.emailTemplate.innerHTML;
+      let text = this.emailTemplate;
+      console.log('[NOTIFICATION-EMAIL] FIND text ', text)
+      text = text.replace(/(<mark class="highlight">|<\/mark>)/gim, '');
+      const newText = text.replace(regex, '<mark class="highlight">$&</mark>');
+      // this.emailTemplate.innerHTML = newText;
+      this.emailTemplate = newText;
+    } else {
+      console.log('[NOTIFICATION-EMAIL] FIND TEXT ON TEXAREA TEXT NOT FOUND')
+    }
+  }
+
 
   refresh() {
     // var inputValue =  (<HTMLInputElement>document.getElementById('text-input')).value;
-    var inputValue = this.assignedRequestTemplate
+    var inputValue = this.emailTemplate
     console.log('REFRESH inputValue', inputValue)
     var parsedHtml = $.parseHTML(inputValue, document, true);
     console.log('REFRESH parsedHtml', parsedHtml)
     $('#output-display').html($(parsedHtml));
   }
 
+  onChangeEmailTempalte(event) {
+    (<HTMLIFrameElement>document.getElementById("iframe-email-template-preview")).srcdoc = this.emailTemplate
+  }
+
   saveEmailNotificationTemplate() {
-    this.projectService.updateEmailTempalte('assignedRequest.html', JSON.stringify(this.assignedRequestTemplate))
+    const save_email_template_btn = <HTMLElement>document.querySelector('.save_email_template');
+    save_email_template_btn.blur();
+   
+    console.log('[NOTIFICATION-EMAIL] - SAVE EMAIL TEMPLATE emailTemplate ', this.emailTemplate)
+    this.projectService.updateEmailTempalte(this.active_template, this.emailTemplate)
+
       .subscribe((res: any) => {
 
-        console.log('saveEmailNotificationTemplate res ', res)
-      })
+        console.log('[NOTIFICATION-EMAIL] - SAVE EMAIL TEMPLATE res ', res)
+      }, (error) => {
+        console.log('[NOTIFICATION-EMAIL] - SAVE EMAIL TEMPLATE error ', error);
+      }, () => {
+        console.log('[NOTIFICATION-EMAIL] - SAVE EMAIL TEMPLATE * COMPLETE *');
+
+      });
 
   }
 
