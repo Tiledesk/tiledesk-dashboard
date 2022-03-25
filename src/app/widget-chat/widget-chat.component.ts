f2e04b89d06f20d8780351d3285c8b02218dc2b5
nicola74 <nicola.lanzilotto@frontiere21.it>
2022-03-25 17:22:24 +0100

    Updates the widget preview graphic to match the changes made to the widget

--- src/app/widget-chat/widget-chat.component.ts
+++ src/app/widget-chat/widget-chat.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'appdashboard-widget-chat',
   templateUrl: './widget-chat.component.html',
   styleUrls: ['./widget-chat.component.scss']
 })
-export class WidgetChatComponent implements OnInit {
+export class WidgetChatComponent implements OnInit, OnChanges {
 
   @Input() primaryColor: string;
   @Input() secondaryColor: string;
@@ -21,10 +21,59 @@ export class WidgetChatComponent implements OnInit {
   @Input() imageUrl: string;
   @Input() currentUserId: string;
   @Input() current_user_name: string;
-  
+  @Input() LABEL_PLACEHOLDER: string;
+  primaryColorRGBA_1: any
+  primaryColorRGBA_050: any
+  linearGradient: any
   constructor() { }
 
   ngOnInit() {
+    // console.log('[WIDGET-CHAT-COMP] primaryColor', this.primaryColor)
+
   }
+
+  ngOnChanges() {
+    console.log('[WIDGET-CHAT-COMP] LABEL_PLACEHOLDER ', this.LABEL_PLACEHOLDER)
+    this.generateLinearGradient(this.primaryColor)
+  }
+
+  generateLinearGradient(primaryColor) {
+    this.primaryColorRGBA_1 = this.hexToRgbA_1(primaryColor)
+    this.primaryColorRGBA_050 = this.hexToRgbA_050(primaryColor)
+    // console.log('[WIDGET-CHAT-COMP] primaryColorRGBA_1', this.primaryColorRGBA_1)
+    // console.log('[WIDGET-CHAT-COMP] primaryColorRGBA_050', this.primaryColorRGBA_050)
+
+    this.linearGradient = 'linear-gradient( 180grad, ' + this.primaryColorRGBA_1 + ', ' + this.primaryColorRGBA_050 + ')';
+    // console.log('[WIDGET-CHAT-COMP] linearGradient', this.linearGradient)
+  }
+
+
+  hexToRgbA_1(hex) {
+    var c;
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+      c = hex.substring(1).split('');
+      if (c.length == 3) {
+        c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+      }
+      c = '0x' + c.join('');
+      return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)';
+    }
+    throw new Error('Bad Hex');
+  }
+
+  hexToRgbA_050(hex) {
+    var c;
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+      c = hex.substring(1).split('');
+      if (c.length == 3) {
+        c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+      }
+      c = '0x' + c.join('');
+      return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.5)';
+    }
+    throw new Error('Bad Hex');
+  }
+
+
 
 }
