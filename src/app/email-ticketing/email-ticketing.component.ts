9040d36d49e3c7a3d919e0e2ec0cd46e2c885dc6
nicola74 <nicola.lanzilotto@frontiere21.it>
2022-05-18 13:03:56 +0200

    Adds email ticketing, which is the ability for the end user to create a ticket by sending an email to the email address available in Settings > Email

--- src/app/email-ticketing/email-ticketing.component.ts
+++ src/app/email-ticketing/email-ticketing.component.ts
@@ -9,15 +9,16 @@ import { URL_getting_started_with_email_ticketing } from '../utils/util';
   styleUrls: ['./email-ticketing.component.scss']
 })
 export class EmailTicketingComponent implements OnInit {
- public projectID: string;
- public isVisibleDEP: boolean
- public IS_OPEN_SETTINGS_SIDEBAR: boolean;
- public email_ticketing_docs_url = URL_getting_started_with_email_ticketing
- public isChromeVerGreaterThan100: boolean;
- public ticketingEmail: string;
- public departments: any;
- public selectedDeptId: string;
- public ticketingEmailDept: string;
+  public projectID: string;
+  public isVisibleDEP: boolean
+  public IS_OPEN_SETTINGS_SIDEBAR: boolean;
+  public email_ticketing_docs_url = URL_getting_started_with_email_ticketing
+  public isChromeVerGreaterThan100: boolean;
+  public ticketingEmail: string;
+  public departments: any;
+  public selectedDeptId: string;
+  public ticketingEmailDept: string;
+  public hasCopiedTicketingEmail: boolean = false
   constructor(
     private deptService: DepartmentService,
     private auth: AuthService,
@@ -31,57 +32,86 @@ export class EmailTicketingComponent implements OnInit {
     this.listenSidebarIsOpened();
   }
 
+  getBrowserVersion() {
+    this.auth.isChromeVerGreaterThan100.subscribe((isChromeVerGreaterThan100: boolean) => {
+      this.isChromeVerGreaterThan100 = isChromeVerGreaterThan100;
+
+    })
+  }
+
   getCurrentProjectAndBuildTicketingEmail() {
     this.auth.project_bs.subscribe((project) => {
       if (project) {
         this.projectID = project._id
-         console.log('[EMAIL-TICKETING] projectID ', this.projectID)
+        this.logger.log('[EMAIL-TICKETING] projectID ', this.projectID)
 
-         this.ticketingEmail = "support@" +  this.projectID + '.tickets.tiledesk.com'
+        this.ticketingEmail = "support@" + this.projectID + '.tickets.tiledesk.com'
       }
     });
   }
 
+  copyTicketingEmail() {
+    const ticketingEmailElem = document.getElementById('ticketing-email') as HTMLInputElement;
+    this.logger.log('onSelectedDeptId - selectedDeptId', ticketingEmailElem);
+    ticketingEmailElem.select();
+    try {
+      document.execCommand('copy');
+      this.hasCopiedTicketingEmail = true
+    } catch (err) {
+      this.logger.error('Fallback: Oops, unable to copy', err);
+    }
+  }
+
+
+
+
 
   getDeptsByProjectId() {
     this.deptService.getDeptsByProjectId().subscribe((departments: any) => {
-     console.log('[EMAIL-TICKETING] - GET DEPTS (FILTERED FOR PROJECT ID)', departments);
+      this.logger.log('[EMAIL-TICKETING] - GET DEPTS (FILTERED FOR PROJECT ID)', departments);
 
       if (departments) {
-        this.departments = [ ]
-        
+        this.departments = []
+
         departments.forEach((dept: any) => {
           // this.logger.log('»»» »»» DEPTS PAGE - DEPT)', dept);
           if (dept && dept.default === false) {
-            console.log('[EMAIL-TICKETING] - GET DEPTS - DEFAULT DEPT ', dept);
+            this.logger.log('[EMAIL-TICKETING] - GET DEPTS - DEFAULT DEPT ', dept);
             this.departments.push(dept)
           }
         });
 
       }
     }, error => {
-     
-      console.error('[EMAIL-TICKETING] (FILTERED FOR PROJECT ID) - ERROR', error);
+
+      this.logger.error('[EMAIL-TICKETING] (FILTERED FOR PROJECT ID) - ERROR', error);
     }, () => {
-      console.log('[EMAIL-TICKETING] (FILTERED FOR PROJECT ID) - COMPLETE')
-     
+      this.logger.log('[EMAIL-TICKETING] (FILTERED FOR PROJECT ID) - COMPLETE')
+
     });
   }
 
   onSelectedDeptId(selectedDeptId: string) {
-    console.log('onSelectedDeptId - selectedDeptId', selectedDeptId);
-
+    this.logger.log('onSelectedDeptId - selectedDeptId', selectedDeptId);
+    this.buildDeptTicketingEmail(selectedDeptId)
   }
 
-  getBrowserVersion() {
-    this.auth.isChromeVerGreaterThan100.subscribe((isChromeVerGreaterThan100: boolean) => { 
-     this.isChromeVerGreaterThan100 = isChromeVerGreaterThan100;
-    //  console.log("[WS-REQUESTS-LIST] isChromeVerGreaterThan100 ",this.isChromeVerGreaterThan100);
-    })
-   }  
+  buildDeptTicketingEmail(selectedDeptId) {
+    // <Department_id>@<Project_id>.tickets.tiledesk.com
+    this.ticketingEmailDept = selectedDeptId + "@" + this.projectID + '.tickets.tiledesk.com'
+  }
+
+  copyTicketingEmailDept() {
+    const ticketingEmailDeptElem = document.getElementById('ticketing-email-dept') as HTMLInputElement;
+    this.logger.log('onSelectedDeptId - selectedDeptId', ticketingEmailDeptElem);
+    ticketingEmailDeptElem.select();
+    document.execCommand('copy');
+  }
 
 
-   listenSidebarIsOpened() {
+
+
+  listenSidebarIsOpened() {
     this.auth.settingSidebarIsOpned.subscribe((isopened) => {
       this.logger.log('[EMAIL-TICKETING] SETTNGS-SIDEBAR is opened (FROM SUBSCRIPTION) ', isopened)
       this.IS_OPEN_SETTINGS_SIDEBAR = isopened
@@ -92,6 +122,8 @@ export class EmailTicketingComponent implements OnInit {
     const url = this.email_ticketing_docs_url;
     window.open(url, '_blank');
   }
+
+
 
 
 }
