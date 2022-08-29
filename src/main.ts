8f4860dba6f632e6a13a3fcff15b38465faf7d43
nicola74 <nicola.lanzilotto@frontiere21.it>
2022-08-29 09:18:45 +0200

    Import hammerjs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// https://stackoverflow.com/questions/41322566/angular-could-not-find-hammerjs
+import 'hammerjs'; 
 
 if (environment.production) {
   enableProdMode();
