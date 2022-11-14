679bcf8661dee9637b42c5d041fe0edc1165c8ea
Dario De Pascalis <dariodepa75@gmail.com>
2022-11-14 16:05:11 +0100

    onboarding: bug fix animation of claims sidebar

--- src/app/create-project-wizard/onboarding/sidebar-claims/sidebar-claims.component.ts
+++ src/app/create-project-wizard/onboarding/sidebar-claims/sidebar-claims.component.ts
@@ -9,23 +9,16 @@ export class SidebarClaimsComponent implements OnInit {
 
   claims = [];
   stars = [];
-  selected: any;
-  timeIntevalSeconds: 5000; 
+  claimVisible: number;
+  // claimHidden: number;
   id;
+  timeIntevalSeconds: 10000; 
+ 
+
+
   constructor() { }
 
   ngOnInit(): void {
-    // let claim;
-    // claim = { stars: 6, message: 'We love what Tiledesk is today. But the best has yet to come',  author:'Andrea Sponziello', role:'Tiledesk CEO'};
-    // this.claims.push(claim);
-    // claim = { stars: 5, message: 'El mejor chatbot que uso, sencillo de manejar',  author:'Alberto P.', role:'CEO'};
-    // this.claims.push(claim);
-    // claim = { stars: 5, message: 'Great product, great support, excited to see more businesses using Tiledesk in the future',  author:'Michael R.', role:'CTO'};
-    // this.claims.push(claim);
-    // claim = { stars: 5, message: 'The best open source live chat, especially with its bot replies',  author:'Peter X.', role:'Co-Founder'};
-    // this.claims.push(claim);
-    // claim = { stars: 5, message: 'Great system with lots of features',  author:'Richard M.', role:'CTO'};
-    // this.claims.push(claim);
     this.claims = [
       { stars: 6, message: 'We love what Tiledesk is today. But the best has yet to come',  author:'Andrea Sponziello', role:'Tiledesk CEO'},
       { stars: 5, message: 'El mejor chatbot que uso, sencillo de manejar',  author:'Alberto P.', role:'CEO'},
@@ -33,10 +26,22 @@ export class SidebarClaimsComponent implements OnInit {
       { stars: 5, message: 'The best open source live chat, especially with its bot replies',  author:'Peter X.', role:'Co-Founder'},
       { stars: 5, message: 'Great system with lots of features',  author:'Richard M.', role:'CTO'}
     ];
+    this.claimVisible = 0;
+    this.stars = Array(this.claims[this.claimVisible].stars);
 
-    this.id = setInterval(() => {
-      this.randomClaim(); 
-    }, 5000);
+    // setTimeout(() => {
+    //   this.claimHidden = 0;
+    //   this.claimVisible = 1;
+    //   console.log('-----> sparisci :::: ');
+    // }, 6000);
+
+
+    if(this.claims.length > 1 ){
+      this.id = setInterval(() => {
+        this.randomClaim(); 
+      }, 10000);
+    }
+   
     
   }
   ngOnDestroy() {
@@ -47,8 +52,16 @@ export class SidebarClaimsComponent implements OnInit {
 
   /** */
   randomClaim(){
-    this.selected = this.claims[Math.floor(Math.random()*this.claims.length)];
-    this.stars = Array(this.selected.stars);
+    let arrayIndex = Array.from(Array(this.claims.length).keys());
+    //console.log('this.arrayIndex::: ', arrayIndex);
+    arrayIndex = arrayIndex.filter((item) => item != this.claimVisible);
+    // console.log('-----> arrayIndex - this.claimActive :::: ', arrayIndex);
+    // this.claimVisible = Math.floor(Math.random()*(arrayIndex.length+1));
+    // this.claimHidden = this.claimVisible;
+    this.claimVisible = arrayIndex[Math.floor(Math.random()*arrayIndex.length)];
+    // console.log('this.claimVisible::: ', this.claimVisible);
+    // console.log('this.claimHidden::: ', this.claimHidden);
+    this.stars = Array(this.claims[this.claimVisible].stars);
   }
 
 
