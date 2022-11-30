66aaa79709f029184c40b9106360a41409fe9d7f
Nicola Lanzilotto <nicola@tilesk.com>
2022-11-30 17:49:49 +0100

    Bots templates

--- src/app/bots/bots-list/bots-sidebar/bots-sidebar.component.ts
+++ src/app/bots/bots-list/bots-sidebar/bots-sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'app/core/auth.service';
 import { LoggerService } from 'app/services/logger/logger.service';
 import { Subject } from 'rxjs';
@@ -17,12 +18,28 @@ export class BotsSidebarComponent implements OnInit {
   IS_OPEN: boolean = true
   private unsubscribe$: Subject<any> = new Subject<any>();
 
+  public BOTS_LIST_ROUTE_IS_ACTIVE: boolean;
+  public BOT_TEMPALTE_ROUTE_IS_ACTIVE: boolean;
+
   constructor(
     private auth: AuthService,
     private logger: LoggerService,
+    public router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.getCurrentRoute()
+    this.listenSidebarIsOpened() 
+    this.getCurrentProject();
+  }
+
+  getCurrentProject() {
+    console.log('[BOTS-SIDEBAR] - CALLING GET CURRENT PROJECT  ', this.project)
+    this.auth.project_bs.subscribe((project) => {
+      this.project = project
+    
+      
+    })
   }
 
   listenSidebarIsOpened() {
@@ -35,6 +52,34 @@ export class BotsSidebarComponent implements OnInit {
   toggletBotsSidebar(IS_OPEN) {
     this.IS_OPEN = IS_OPEN;
     this.auth.togglebotsSidebar(IS_OPEN)
+  }
+
+  getCurrentRoute() {
+    this.route = this.router.url
+    if (this.route.indexOf('/bots') !== -1) {
+      this.BOTS_LIST_ROUTE_IS_ACTIVE = true
+      console.log('[BOTS-SIDEBAR] - BOTS_LIST_ROUTE_IS_ACTIVE  ', this.BOTS_LIST_ROUTE_IS_ACTIVE)
+    } else {
+      this.BOTS_LIST_ROUTE_IS_ACTIVE = false
+      // console.log('[NATIVE-BOT-SIDEBAR] - GENERAL_ROUTE_IS_ACTIVE  ', this.GENERAL_ROUTE_IS_ACTIVE)
+    }
+
+    if (this.route.indexOf('/bots/templates') !== -1) {
+      this.BOT_TEMPALTE_ROUTE_IS_ACTIVE = true
+      console.log('[BOTS-SIDEBAR] - BOT_TEMPALTE_ROUTE_IS_ACTIVE  ', this.BOT_TEMPALTE_ROUTE_IS_ACTIVE)
+    } else {
+      this.BOT_TEMPALTE_ROUTE_IS_ACTIVE = false
+      console.log('[BOTS-SIDEBAR] - BOT_TEMPALTE_ROUTE_IS_ACTIVE  ', this.BOT_TEMPALTE_ROUTE_IS_ACTIVE)
+    }
+  }
+
+
+  goToBotsList() {
+    this.router.navigate(['project/' + this.project._id + '/bots']);
+  }
+
+  goToBotTemplates() {
+    this.router.navigate(['project/' + this.project._id + '/bots/templates']);
   }
 
 }
