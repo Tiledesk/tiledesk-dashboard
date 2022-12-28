e25e486c4312c827fa9c70619e2ebe35da3e6b01
Nicola Lanzilotto <nicola@tilesk.com>
2022-12-28 10:14:31 +0100

    Chatbot design studio

--- src/app/chatbot-design-studio/cds-dashboard/panel-intent-list/panel-intent-list.component.ts
+++ src/app/chatbot-design-studio/cds-dashboard/panel-intent-list/panel-intent-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { FaqService } from './../../../services/faq.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Faq } from 'app/models/faq-model';
 
 @Component({
   selector: 'appdashboard-panel-intent-list',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelIntentListComponent implements OnInit {
 
-  constructor() { }
+  @Input() id_faq_kb: string;
+
+  predefined_faqs = [];
+  faqs = [];
+
+  constructor(private faqService: FaqService) { }
 
   ngOnInit(): void {
+    console.log("Panel-intent-list ngOnInit()")
+    console.log("Selected chatbot: ", this.id_faq_kb);
+    this.getAllIntents(this.id_faq_kb);
+
+  }
+
+  getAllIntents(id_faq_kb) {
+
+    this.faqService.getAllFaqByFaqKbId(id_faq_kb).subscribe((faqs: any) => {
+      console.log("faqs: ", faqs);
+      this.faqs = faqs;
+
+      let faq_start = faqs.find(o => o.name === '//start');
+      console.log("faq_start: ", faq_start)
+
+
+    }), (error) => {
+      console.error("error: ", error)
+    }, () => {
+      console.log("End.")
+    }
   }
 
 }
