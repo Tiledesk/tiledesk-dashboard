78967ba39fc53f3aeeec28ef3295415058780a55
Nicola Lanzilotto <nicola@tilesk.com>
2023-01-19 11:46:39 +0100

    Merge

--- src/app/services/faq.service.ts
+++ src/app/services/faq.service.ts
@@ -371,12 +371,9 @@ public _getAllFaqByFaqKbId(id_faq_kb: string): Observable<Intent[]> {
     this.logger.log('[FAQ-SERV] UPDATE FAQ - ID ', id);
     let url = this.FAQ_URL + id;
     this.logger.log('[FAQ-SERV] UPDATE FAQ - PUT URL ', url);
-<<<<<<< HEAD
 
     const body = { 'question': question, 'answer': answer, 'intent_display_name': intentname, 'form': intentform, 'webhook_enabled': faqwebhookenabled };
-=======
-    const body = { 'question': question, 'answer': answer, 'intent_display_name': intentname, 'form':intentform, 'webhook_enabled': faqwebhookenabled };
->>>>>>> chatbot-dashboard
+   
     this.logger.log('[FAQ-SERV] UPDATE FAQ - PUT REQUEST BODY ', body);
     return this._httpClient
       .put(url, JSON.stringify(body), httpOptions)
