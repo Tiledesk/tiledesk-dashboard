362906628f9377ad65a7cd13d07b549ca260195d
Nicola Lanzilotto <nicola@tilesk.com>
2023-01-27 19:17:52 +0100

    Create chatbot

--- src/app/bots/create-chatbot/create-chatbot.component.ts
+++ src/app/bots/create-chatbot/create-chatbot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FaqKbService } from 'app/services/faq-kb.service';
 
 @Component({
   selector: 'appdashboard-create-chatbot',
@@ -7,14 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateChatbotComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private faqKbService: FaqKbService,
+  ) { }
 
   ngOnInit(): void {
     this.getCommunityTemplatesAndFilterForStartChatbot()
   }
 
-  getCommunityTemplatesAndFilterForStartChatbot() {
-    
-  }
+ 
+    getCommunityTemplatesAndFilterForStartChatbot() {
+      // this.showSpinner = true;
+      this.faqKbService.getCommunityTemplates().subscribe((res: any) => {
+  
+        if (res) {
+          const communityTemplates = res
+          console.log('[CREATE-CHATBOT] communityTemplates' ,communityTemplates);
+          // communityTemplates.forEach(template => {
+          //   if () 
+            
+          // });
+
+          const startChatBot = communityTemplates.filter((el) => {
+            return el.mainCategory === "System" && el.tags.includes('start-chatbot')
+          });
+  
+          console.log('[CREATE-CHATBOT] startChatBot' , startChatBot);
+        }
+  
+      }, (error) => {
+        console.error('[CREATE-CHATBOT] GET COMMUNITY TEMPLATES ERROR ', error);
+      
+      }, () => {
+        console.log('[CREATE-CHATBOT] GET COMMUNITY TEMPLATES COMPLETE');
+      
+        // this.generateTagsBackground(this.templates)
+      });
+  
+    }
+  
 
 }
