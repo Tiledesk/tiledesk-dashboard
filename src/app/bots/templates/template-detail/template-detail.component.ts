49040ba283157dd44cbec7bcf1a371500c6379f2
Nicola Lanzilotto <nicola@tilesk.com>
2023-01-29 09:04:18 +0100

    Minor improvents

--- src/app/bots/templates/template-detail/template-detail.component.ts
+++ src/app/bots/templates/template-detail/template-detail.component.ts
@@ -269,11 +269,11 @@ export class TemplateDetailComponent implements OnInit {
 
   goToBotDetails() {
     this.logger.log('[TEMPLATE DETAIL] GO TO  BOT DETAILS - isDevMode() ', isDevMode());
-      if (isDevMode() === false) { 
-        this.router.navigate(['project/' + this.projectid + '/tilebot/intents/', this.botid, 'tilebot']);
-      } else {
+      // if (isDevMode() === false) { 
+      //   this.router.navigate(['project/' + this.projectid + '/tilebot/intents/', this.botid, 'tilebot']);
+      // } else {
         this.router.navigate(['project/' + this.project._id + '/cds/', this.botid]);
-      }
+      // }
     
     this.closeDialog();
     // this.closeCreateBotInfoModal();
