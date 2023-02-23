772347d1135a6d72beb8d7d587db355b43c225d2
Gabriele Panico <gabriele.panico@gmail.com>
2023-02-23 13:09:03 +0100

    bug-fixed: select not save current value

--- src/app/chatbot-design-studio/cds-base-element/select/select.component.ts
+++ src/app/chatbot-design-studio/cds-base-element/select/select.component.ts
@@ -32,14 +32,14 @@ export class SelectComponent implements OnInit {
     //   console.log('itemmmm selectedddd-->', this.itemSelected, this.items)
     //   this.itemSelected = this.items.find(el => el[this.bindValueSelect] === this.itemSelected)
     // }
-    // this.itemSelected = this.items.find(el => el[this.bindValueSelect] === this.itemSelected)
+    this.itemSelected = this.items.find(el => el[this.bindValueSelect] === this.itemSelected)[this.bindValueSelect]
   }
 
   onChangeActionButton(event) {
-    console.log("[SELECT BASE ELEMENT] onChangeActionButton event: ", event)
+    console.log("[SELECT BASE ELEMENT] onChangeActionButton event: ", event, this.items)
     // this.itemSelected = event;
-    this.itemSelected = this.items.find(el => el[this.bindValueSelect] === event)
-    this.onSelected.emit(this.itemSelected)
+    this.itemSelected = event[this.bindValueSelect];
+    this.onSelected.emit(event)
   }
 
 }
