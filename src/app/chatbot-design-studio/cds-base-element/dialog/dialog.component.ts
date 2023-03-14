f0f9013750dfe73452136bc104a955909f4141d2
Dario De Pascalis <dariodepa75@gmail.com>
2023-03-14 20:33:33 +0100

    added regex test to input custom attributes

--- src/app/chatbot-design-studio/cds-base-element/dialog/dialog.component.ts
+++ src/app/chatbot-design-studio/cds-base-element/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
@@ -9,33 +9,60 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 })
 export class DialogComponent implements OnInit {
 
-  actionHideMessageFormGroup: FormGroup;
+  // actionHideMessageFormGroup: FormGroup;
+  // textInput: string;
+  btnDisabled: boolean = true;
   
   constructor(
-    private formBuilder: FormBuilder,
+    // private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   ngOnInit(): void {
-    this.initialize();
+    // this.initialize();
   }
     
-  private initialize(){
-    this.actionHideMessageFormGroup = this.buildForm();
-    this.actionHideMessageFormGroup.valueChanges.subscribe(form => {
-      if(form && (form.text !== '')){
-        this.data.name = Object.assign(this.data.name, this.actionHideMessageFormGroup.value);
-      }
-    })
-  }
+  // private initialize(){
+  //   let that = this;
+  //   this.textInput = this.data.text;
+  //   this.actionHideMessageFormGroup = this.buildForm();
+  //   this.actionHideMessageFormGroup.valueChanges.subscribe(form => {
+  //     console.log('actionHideMessageFormGroup: ', that.actionHideMessageFormGroup);
+  //     if(form && (form.text !== '') && that.actionHideMessageFormGroup.status === 'VALID'){
+  //       // console.log('assegno!!', this.actionHideMessageFormGroup);
+  //       Object.assign(that.textInput, that.actionHideMessageFormGroup.value);
+  //       that.btnDisabled = false;
+  //     } else {
+  //       that.btnDisabled = true;
+  //     }
+  //   })
+  // }
+
+
+  // private buildForm(): FormGroup {
+  //   //RegExp(/^[a-zA-Z_]*[a-zA-Z_]+[a-zA-Z0-9_]*$/gm)
+  //   const regexPattern = "^[a-zA-Z0-9_]*$";
+  //   return this.formBuilder.group({
+  //     text: ['', [Validators.required, Validators.pattern(new RegExp(regexPattern))]]
+  //   })
+  // }
 
 
   
-  private buildForm(): FormGroup{
-    return this.formBuilder.group({
-      text: ['', Validators.required]
-    })
+
+  onChangeTextInput($event):void {
+    console.log('onChangeTextInput', $event);
+    const regexPattern = "/^[a-zA-Z0-9_]*$/";
+    let REGEX = new RegExp(regexPattern.replace(/\//gi, ''));
+    // this.logger.log('[TILEBOT-EDIT-ADD] checkFields nameRGEX REGEX ', REGEX)
+    if(REGEX.test(this.data.text) && this.data.text !== ''){
+      console.log('ok');
+      this.btnDisabled = false;
+    } else {
+      console.log('error');
+      this.btnDisabled = true;
+    }
   }
 
   onAddCustomAttribute(): void {
@@ -43,14 +70,8 @@ export class DialogComponent implements OnInit {
     // this.dialogRef.close();
   }
 
-  //RegExp(/^[a-zA-Z_]*[a-zA-Z_]+[a-zA-Z0-9_]*$/gm)
-
   onCloseDialog(): void {
     this.dialogRef.close();
-  }
-
-  onChange(){
-    // verifico regex
   }
 
 
--- src/app/chatbot-design-studio/cds-dashboard/panel-intent-detail/actions/action-json-condition/variable-list/variable-list.component.ts
+++ src/app/chatbot-design-studio/cds-dashboard/panel-intent-detail/actions/action-json-condition/variable-list/variable-list.component.ts
@@ -56,10 +56,10 @@ export class VariableListComponent implements OnInit {
     var that = this;
     const dialogRef = this.dialog.open(DialogComponent, {
       panelClass: 'custom-dialog-container',
-      data: {name: '', value: ''}
+      data: {text: ''}
     });
     dialogRef.afterClosed().subscribe(result => {
-      // console.log(`Dialog result: ${result}`);
+      console.log(`Dialog result: ${result}`);
       let variable = {name: result, value: result};
       // variableList.userDefined.push(variable);
       that.variableListUserDefined.push(variable);
