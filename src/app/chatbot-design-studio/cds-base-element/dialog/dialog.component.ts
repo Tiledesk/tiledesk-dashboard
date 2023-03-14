cbac035b3b624f5ce42b8f94b69fa9eb02023416
Dario De Pascalis <dariodepa75@gmail.com>
2023-03-14 12:41:48 +0100

    impl. regex "add custom attr"

--- src/app/chatbot-design-studio/cds-base-element/dialog/dialog.component.ts
+++ src/app/chatbot-design-studio/cds-base-element/dialog/dialog.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
@@ -8,17 +9,41 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 })
 export class DialogComponent implements OnInit {
 
+  actionHideMessageFormGroup: FormGroup;
+  
   constructor(
+    private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<DialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   ngOnInit(): void {
+    this.initialize();
+  }
+    
+  private initialize(){
+    this.actionHideMessageFormGroup = this.buildForm();
+    this.actionHideMessageFormGroup.valueChanges.subscribe(form => {
+      if(form && (form.text !== '')){
+        this.data.name = Object.assign(this.data.name, this.actionHideMessageFormGroup.value);
+      }
+    })
+  }
+
+
+  
+  private buildForm(): FormGroup{
+    return this.formBuilder.group({
+      text: ['', Validators.required]
+    })
   }
 
   onAddCustomAttribute(): void {
     // console.log('onAddCustomAttribute');
     // this.dialogRef.close();
   }
+
+  //RegExp(/^[a-zA-Z_]*[a-zA-Z_]+[a-zA-Z0-9_]*$/gm)
 
   onCloseDialog(): void {
     this.dialogRef.close();
