2641bfe5c15ac02b24c6e3a17560f33bed9e0d5c
Nicola Lanzilotto <nicola@tilesk.com>
2023-05-04 12:00:37 +0200

    Custom properties

--- src/app/components/shared/contact-info/contact-info.component.ts
+++ src/app/components/shared/contact-info/contact-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'app/core/auth.service';
 import { NotifyService } from 'app/core/notify.service';
@@ -27,6 +27,7 @@ export class ContactInfoComponent implements OnInit, OnChanges, OnDestroy {
     public auth: AuthService,
     public contactsService: ContactsService,
     public notify: NotifyService,
+    public renderer: Renderer2
   ) { }
 
 
@@ -57,7 +58,7 @@ export class ContactInfoComponent implements OnInit, OnChanges, OnDestroy {
         console.log('contactPhone ', this.contactPhone)
       }
 
-      
+
 
     }
   }
@@ -125,7 +126,7 @@ export class ContactInfoComponent implements OnInit, OnChanges, OnDestroy {
     this.updateContactemail(this.contact_details._id, this.contactNewEmail);
   }
 
- 
+
 
   updateContactemail(contatid: string, contatemail: string) {
     this.contactsService.updateLeadEmail(contatid, contatemail)
@@ -162,12 +163,12 @@ export class ContactInfoComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  removePhoneAnUpdateContact(){
-    this.contactPhone =""
+  removePhoneAnUpdateContact() {
+    this.contactPhone = ""
     this.updateContactPhone(this.contact_details._id, this.contactPhone)
   }
 
- 
+
   updateContactCompany(contactid, contactcompany) {
     this.contactsService.updateLeadCompany(
       contactid,
@@ -210,6 +211,71 @@ export class ContactInfoComponent implements OnInit, OnChanges, OnDestroy {
     })
 
   }
+
+  _addInput() {
+    let ulElm = <HTMLElement>document.querySelector('.contact-info-list');
+    let li = document.createElement("li");
+    let children = ulElm.children.length + 1
+    console.log('ulElm', ulElm)
+    console.log('children', children)
+    li.setAttribute("id", "element" + children)
+    let div = document.createElement('div');
+    div.className = "contact-row"
+    li.appendChild(div);
+    let span = document.createElement('span');
+    div.appendChild(span);
+
+    let svg = `<svg xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 0 24 24" fill="none" style="min-width: 22px; min-height: 22px;"><path  d="M0 0h24v24H0V0z" fill="none"></path><path d="M9.17 6l2 2H20v10H4V6h5.17M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"></path></svg>`
+    const parseSvgString = svgString =>
+      new DOMParser().parseFromString(svgString, 'image/svg+xml')
+        .querySelector('svg')
+    span.appendChild(parseSvgString(svg));
+    // li.appendChild(document.createTextNode("Element "+children));
+    let div2 = document.createElement('div');
+    li.appendChild(div2);
+    ulElm.appendChild(li)
+  }
+
+  addInput() {
+    let ulElm = <HTMLElement>document.querySelector('.contact-info-list');
+    let li = this.renderer.createElement("li");
+    let children = ulElm.children.length + 1
+    li.setAttribute("id", "element" + children)
+    let customPropertyTemplateAsString =
+      `<div class="contact-row">
+   <span>
+     <svg xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 0 24 24" style="min-width: 22px; min-height: 22px;" fill="none"
+       matTooltipClass="custom-mat-tooltip" matTooltip="{{ 'Company' | translate }}" #tooltip="matTooltip"
+       matTooltipPosition='right' matTooltipHideDelay="100">
+       <path d="M0 0h24v24H0V0z" fill="none" />
+       <path
+         d="M12 7V3H2v18h20V7H12zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10zm-2-8h-2v2h2v-2zm0 4h-2v2h2v-2z" />
+     </svg>
+   </span>
+
+   <div class="form-group" style="margin-top: 2px;"
+     [ngStyle]="{'width':CHAT_PANEL_MODE === true ? 'unset' : '100%' }">
+     <input type="text" name="company" data-id="company" class="form-control input-md" [(ngModel)]="contactCompany"
+       placeholder="Company..." maxlength="255" (keydown.enter)='editContactCompany()' autocomplete="off"
+       style="padding-bottom: 2px;" [ngStyle]="{'width':CHAT_PANEL_MODE === true ? '200px' : '100%' }">
+   </div>
+   <div class="action-btns">
+     <button type="button" data-id="company" (click)="removeCompanyAnUpdateContact()">
+       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+         style="min-width: 20px; min-height: 20px;">
+         <path
+           d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
+         </path>
+         <path d="M0 0h24v24H0z" fill="none"></path>
+       </svg>
+     </button>
+   </div>
+ </div>`
+
+    li.innerHTML = customPropertyTemplateAsString;
+    
+  }
+
 
 
 }
