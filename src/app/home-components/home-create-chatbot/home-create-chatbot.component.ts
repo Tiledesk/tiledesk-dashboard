cbd594e5e4fd5319c7fb4e1f160ff08642e59bc0
Nicola Lanzilotto <nicola@tilesk.com>
2023-06-09 10:08:05 +0200

    New home

--- src/app/home-components/home-create-chatbot/home-create-chatbot.component.ts
+++ src/app/home-components/home-create-chatbot/home-create-chatbot.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'app/core/auth.service';
+import { LoggerService } from 'app/services/logger/logger.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators'
 
 @Component({
   selector: 'appdashboard-home-create-chatbot',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeCreateChatbotComponent implements OnInit {
 
-  constructor() { }
+  private unsubscribe$: Subject<any> = new Subject<any>();
+  projectId: string;
+  constructor(
+    public auth: AuthService,
+    private logger: LoggerService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getCurrentProjectAndInit();
+  }
+
+  getCurrentProjectAndInit() {
+    this.auth.project_bs
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe((project) => {
+        console.log('[HOME-CREATE-CHATBOT] $UBSCIBE TO PUBLISHED PROJECT - RES  ', project)
+
+        if (project) {
+          
+          this.projectId = project._id
+        }
+      }, (error) => {
+        this.logger.error('[HOME-CREATE-CHATBOT] $UBSCIBE TO PUBLISHED PROJECT - ERROR ', error);
+
+      }, () => {
+        this.logger.log('[HOME-CREATE-CHATBOT] $UBSCIBE TO PUBLISHED PROJECT * COMPLETE *');
+      });
+  }
+
+
+  goToTemplates() {
+    this.router.navigate(['project/' + this.projectId + '/bots/templates/all']);
   }
 
 }
