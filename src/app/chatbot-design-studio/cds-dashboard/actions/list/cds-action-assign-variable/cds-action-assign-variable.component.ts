b6d9a8c4f4c698847e11496875cc50021747e39d
Gabriele Panico <gabriele.panico@gmail.com>
2023-08-30 15:42:55 +0200

    bug-fixed: cds-action-assign-variable not updated on the fly

--- src/app/chatbot-design-studio/cds-dashboard/actions/list/cds-action-assign-variable/cds-action-assign-variable.component.ts
+++ src/app/chatbot-design-studio/cds-dashboard/actions/list/cds-action-assign-variable/cds-action-assign-variable.component.ts
@@ -13,8 +13,9 @@ export class CdsActionAssignVariableComponent implements OnInit, OnChanges {
     @Input() previewMode: boolean = true;
     displaySetOperationPlaceholder:boolean = true
 
-    variables: Array<string> = [];
-    listOfFunctions: Array<{ name: string, value: string, icon?: string }> = [];
+    listOfMathOperators: Array<{ name: string, value: string, src?: string }> = [];
+    listOfFunctions: Array<{ name: string, value: string, src?: string }> = [];
+
 
     TYPE_FUNCTION_LIST_FOR_VARIABLES = TYPE_FUNCTION_LIST_FOR_VARIABLES
     TYPE_MATH_OPERATOR_LIST = TYPE_MATH_OPERATOR_LIST
@@ -33,45 +34,58 @@ export class CdsActionAssignVariableComponent implements OnInit, OnChanges {
         const operators = this.action.operation.operators
         console.log('[CDS-ACTION-ASSIGN-VARIABLE] operands ', operands)
         console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators ', operators)
-        console.log('[CDS-ACTION-ASSIGN-VARIABLE] list  ', TYPE_MATH_OPERATOR_LIST['addAsStrings'])
         
-        for (let i = 0; i < operands.length; i++) {
-            // for (let key in TYPE_FUNCTION_LIST_FOR_VARIABLES) {
-            //     let _function = operands[i].function
-            //     // console.log('[CDS-ACTION-ASSIGN-VARIABLE] operand _function (2)' , _function)
-            //     // console.log('[CDS-ACTION-ASSIGN-VARIABLE]  TYPE_FUNCTION_LIST_FOR_VARIABLES key ' ,TYPE_FUNCTION_LIST_FOR_VARIABLES[key])
-            //     if (_function === TYPE_FUNCTION_LIST_FOR_VARIABLES[key].type) {
-            //         console.log('[CDS-ACTION-ASSIGN-VARIABLE] >>>>>> operand  ', operands[i])
-            //         operands[i]['functionName'] = TYPE_FUNCTION_LIST_FOR_VARIABLES[key].name
-            //     }
-            // }
-            // console.log('[CDS-ACTION-ASSIGN-VARIABLE] operands -xx', operands[i])
-            // if (operands[i].value === '') {
-            //     this.displaySetOperationPlaceholder = false
-            //     console.log('[CDS-ACTION-ASSIGN-VARIABLE] displaySetOperationPlaceholder -xx', this.displaySetOperationPlaceholder)
-            // }
-            // for (let j = 0; j < operators.length; j++) {
-            //     // console.log('[CDS-ACTION-ASSIGN-VARIABLE] operator ---xx>', operators)
-            //     if (operands.length - 1) {
-            //         console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators j -xx>', operators[j])
-            //         console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators i -xx>', operators[i])
-            //         console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators i  TYPE_MATH_OPERATOR[operators[i]] -xx>', TYPE_MATH_OPERATOR[operators[i]])
-            //         // operands[i]['operator'] = TYPE_MATH_OPERATOR[operators[j]]
-            //         for (let key in TYPE_MATH_OPERATOR_LIST) {
-            //             if (operators[i] === TYPE_MATH_OPERATOR_LIST[key].type) {
-            //                 operands[i]['operator'] = TYPE_MATH_OPERATOR_LIST[key].name
-            //             }
-            //         }
+        // for (let i = 0; i < operands.length; i++) {
+        //     for (let key in TYPE_FUNCTION_LIST_FOR_VARIABLES) {
+        //         let _function = operands[i].function
+        //         // console.log('[CDS-ACTION-ASSIGN-VARIABLE] operand _function (2)' , _function)
+        //         // console.log('[CDS-ACTION-ASSIGN-VARIABLE]  TYPE_FUNCTION_LIST_FOR_VARIABLES key ' ,TYPE_FUNCTION_LIST_FOR_VARIABLES[key])
+        //         if (_function === TYPE_FUNCTION_LIST_FOR_VARIABLES[key].type) {
+        //             console.log('[CDS-ACTION-ASSIGN-VARIABLE] >>>>>> operand  ', operands[i])
+        //             operands[i]['functionName'] = TYPE_FUNCTION_LIST_FOR_VARIABLES[key].name
+        //         }
+        //     }
+        //     console.log('[CDS-ACTION-ASSIGN-VARIABLE] operands -xx', operands[i])
+        //     if (operands[i].value === '') {
+        //         this.displaySetOperationPlaceholder = false
+        //         console.log('[CDS-ACTION-ASSIGN-VARIABLE] displaySetOperationPlaceholder -xx', this.displaySetOperationPlaceholder)
+        //     }
+        //     for (let j = 0; j < operators.length; j++) {
+        //         // console.log('[CDS-ACTION-ASSIGN-VARIABLE] operator ---xx>', operators)
+        //         if (operands.length - 1) {
+        //             console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators j -xx>', operators[j])
+        //             console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators i -xx>', operators[i])
+        //             console.log('[CDS-ACTION-ASSIGN-VARIABLE] operators i  TYPE_MATH_OPERATOR[operators[i]] -xx>', TYPE_MATH_OPERATOR[operators[i]])
+        //             // operands[i]['operator'] = TYPE_MATH_OPERATOR[operators[j]]
+        //             for (let key in TYPE_MATH_OPERATOR_LIST) {
+        //                 if (operators[i] === TYPE_MATH_OPERATOR_LIST[key].type) {
+        //                     operands[i]['operator'] = TYPE_MATH_OPERATOR_LIST[key].name
+        //                 }
+        //             }
 
-            //     }
-            // }
+        //         }
+        //     }
+        // }
+
+    }
+
+    getValue(key: string, type: 'operands' | 'operators'): any{
+        // return TYPE_FUNCTION_LIST_FOR_VARIABLES[key]
+        if(type === 'operands'){
+            return this.listOfFunctions.filter(el => el.value === key)[0]
         }
-
+        if(type === 'operators'){
+            return this.listOfMathOperators.filter(el => el.value === key)[0]
+        }
+       
     }
 
     initialize() {
         for (let key in TYPE_FUNCTION_LIST_FOR_VARIABLES) {
-            this.listOfFunctions.push({ name: TYPE_FUNCTION_LIST_FOR_VARIABLES[key].name, value: TYPE_FUNCTION_LIST_FOR_VARIABLES[key].type });
+            this.listOfFunctions.push({ name: TYPE_FUNCTION_LIST_FOR_VARIABLES[key].name, value: TYPE_FUNCTION_LIST_FOR_VARIABLES[key].type, src:  TYPE_FUNCTION_LIST_FOR_VARIABLES[key].src});
+        }
+        for (let key in TYPE_MATH_OPERATOR_LIST) {
+            this.listOfMathOperators.push({ name: TYPE_MATH_OPERATOR_LIST[key].name, value: TYPE_MATH_OPERATOR_LIST[key].type, src:  TYPE_MATH_OPERATOR_LIST[key].src});
         }
     }
 
