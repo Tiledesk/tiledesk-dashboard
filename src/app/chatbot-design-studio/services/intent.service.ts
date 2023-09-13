45953172863153541d9b80369d602355c5e0ef06
Gabriele Panico <gabriele.panico@gmail.com>
2023-09-13 10:25:03 +0200

    added: new askgpt + kb page + new label translations

--- src/app/chatbot-design-studio/services/intent.service.ts
+++ src/app/chatbot-design-studio/services/intent.service.ts
@@ -700,7 +700,6 @@ export class IntentService {
       action.question = '${last_user_text}'
       action.assignReplyTo = 'gpt_reply';
       action.assignSourceTo = 'gpt_source';
-      action.assignSuccessTo = 'gpt_success';
     }
     console.log('ho creato nuova action ', action);
     return action;
