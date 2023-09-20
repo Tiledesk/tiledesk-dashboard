62e670254030fd7941a73194b612fec22afdd4ca
Gabriele Panico <gabriele.panico@gmail.com>
2023-09-20 17:41:47 +0200

    added: custom menu-context component --> start

---
+++ src/app/chatbot-design-studio/cds-dashboard/cds-context-menu/cds-context-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cds-context-menu',
+  templateUrl: './cds-context-menu.component.html',
+  styleUrls: ['./cds-context-menu.component.scss']
+})
+export class CdsContextMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
