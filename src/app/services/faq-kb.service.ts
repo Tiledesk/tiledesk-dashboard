0469975eff251f5557a5e779a488037c7700d60a
Nicola Lanzilotto <nicola@tiledesk.com>
2023-12-22 11:59:40 +0100

    Commented log

--- src/app/services/faq-kb.service.ts
+++ src/app/services/faq-kb.service.ts
@@ -66,7 +66,7 @@ export class FaqKbService {
       // debugger
 
       if (this.project) {
-         console.log('00 -> FAQKB SERVICE project ID from AUTH service subscription  ', this.project._id)
+        //  console.log('00 -> FAQKB SERVICE project ID from AUTH service subscription  ', this.project._id)
         this.FAQKB_URL = this.SERVER_BASE_PATH + this.project._id + '/faq_kb/'
 
 
