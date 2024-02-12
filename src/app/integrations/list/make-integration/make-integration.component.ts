c8d8aaa2abc95db6421c25d491e0f791eb3920e1
Nicola Lanzilotto <nicola@tiledesk.com>
2024-02-12 12:32:31 +0100

    Adds integrations

---
+++ src/app/integrations/list/make-integration/make-integration.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { LoggerService } from 'app/services/logger/logger.service';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'make-integration',
+  templateUrl: './make-integration.component.html',
+  styleUrls: ['./make-integration.component.scss']
+})
+export class MakeIntegrationComponent implements OnInit, OnChanges{
+
+  @Input() integration: any;
+  @Output() onUpdateIntegration = new EventEmitter;
+  @Output() onDeleteIntegration = new EventEmitter;
+
+  
+ translateparams: any;
+
+  constructor(
+    private logger: LoggerService,
+    private translate: TranslateService,
+  ) { }
+
+  ngOnInit(): void {
+    console.log("[INT-Make] integration (oninit)", this.integration);
+    this.translateparams = { intname: 'Make' };
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("[INT-Make] integration (onchanges) changes", changes);
+    console.log("[INT-Make] integration (onchanges) integration", this.integration);
+
+  }
+
+}
