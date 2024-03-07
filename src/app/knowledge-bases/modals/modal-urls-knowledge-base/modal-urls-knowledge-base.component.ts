1c2bb5714917e3d516c752e98b4fff809ae14041
Dario De Pascalis <dariodepa75@gmail.com>
2024-03-07 16:42:47 +0100

    ✅ changed data-source in data source ✅ changed import URLs with LIMIT urls and css == sitemap

--- src/app/knowledge-bases/modals/modal-urls-knowledge-base/modal-urls-knowledge-base.component.ts
+++ src/app/knowledge-bases/modals/modal-urls-knowledge-base/modal-urls-knowledge-base.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { KB_LIMIT_CONTENT } from 'app/utils/util';
 
 @Component({
   selector: 'modal-urls-knowledge-base',
@@ -11,11 +12,12 @@ export class ModalUrlsKnowledgeBaseComponent implements OnInit {
   @Output() saveKnowledgeBase = new EventEmitter();
   @Output() closeBaseModal = new EventEmitter();
 
+  KB_LIMIT_CONTENT = KB_LIMIT_CONTENT;
   kbForm: FormGroup;
   buttonDisabled: boolean = true;
-
   list = [];
   content: string;
+  countSitemap: number;
 
   // kb: KB = {
   //   _id: null,
@@ -30,6 +32,14 @@ export class ModalUrlsKnowledgeBaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.kbForm = this.createConditionGroup();
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    // console.log('ModalSiteMapComponent changes: ', changes);
+    // if(this.listSitesOfSitemap.length > 0){
+    //   this.listOfUrls = this.listSitesOfSitemap.join('\n');
+    //   this.countSitemap = this.listSitesOfSitemap.length;
+    // } 
   }
 
   createConditionGroup(): FormGroup {
@@ -48,10 +58,17 @@ export class ModalUrlsKnowledgeBaseComponent implements OnInit {
     } else {
       this.buttonDisabled = true;
     }
+    let listSitesOfSitemap = this.content.split("\n").filter(function(row) {
+      return row.trim() !== '';
+    });
+    this.countSitemap = listSitesOfSitemap.length;
   }
 
   onSaveKnowledgeBase(){
-    const arrayURLS = this.content.split('\n');
+    //const arrayURLS = this.content.split('\n');
+    const arrayURLS = this.content.split("\n").filter(function(row) {
+      return row.trim() !== '';
+    });
     let body = {
       'list': arrayURLS
     }
@@ -59,6 +76,7 @@ export class ModalUrlsKnowledgeBaseComponent implements OnInit {
   }
 
   onCloseBaseModal() {
+    this.countSitemap = 0;
     this.closeBaseModal.emit();
   }
 }
