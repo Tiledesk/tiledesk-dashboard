856d96fb7669b33b3f59e35bf7a42e08fb529b5d
Nicola Lanzilotto <nicola@tiledesk.com>
2024-05-05 19:23:28 +0200

    Adds, in the Magento section of the Installation page, the link to the Help center documentation

--- src/app/components/widget-installations/magento-installation/magento-installation.component.ts
+++ src/app/components/widget-installations/magento-installation/magento-installation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BrandService } from 'app/services/brand.service';
+import { URL_install_tiledesk_on_magento } from 'app/utils/util';
 
 @Component({
   selector: 'appdashboard-magento-installation',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./magento-installation.component.scss']
 })
 export class MagentoInstallationComponent implements OnInit {
-
-  constructor() { }
+  URL_install_tiledesk_on_magento = URL_install_tiledesk_on_magento;
+  public hideHelpLink: boolean;
+  constructor(
+    public brandService: BrandService
+  ) { 
+    const brand = brandService.getBrand(); 
+    this.hideHelpLink= brand['DOCS'];
+  }
 
   ngOnInit(): void {
   }
