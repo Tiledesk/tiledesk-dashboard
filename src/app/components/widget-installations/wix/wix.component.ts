0b45dcb372ce83d4fdb532a609dbd9448b221f8a
Nicola Lanzilotto <nicola@tiledesk.com>
2024-05-05 12:54:55 +0200

    Adds link to help center article "How to Install the Tiledesk Live Chat Widget on a WIX website

--- src/app/components/widget-installations/wix/wix.component.ts
+++ src/app/components/widget-installations/wix/wix.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BrandService } from 'app/services/brand.service';
+import { URL_install_tiledesk_on_wix } from 'app/utils/util';
 
 @Component({
   selector: 'appdashboard-wix',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wix.component.scss']
 })
 export class WixComponent implements OnInit {
+  URL_install_tiledesk_on_wix = URL_install_tiledesk_on_wix 
+  public hideHelpLink: boolean;
 
-  constructor() { }
+  constructor(
+    public brandService: BrandService
+  ) { 
+    const brand = brandService.getBrand(); 
+    this.hideHelpLink= brand['DOCS'];
+  }
 
   ngOnInit(): void {
   }
