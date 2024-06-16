14ab48e0f380218cb54a7023d3897c543168d928
Nicola Lanzilotto <nicola@tiledesk.com>
2024-06-16 18:16:05 +0200

    Hide / show project Extreme Measures

--- src/app/project-edit-add/project-edit-add.component.ts
+++ src/app/project-edit-add/project-edit-add.component.ts
@@ -222,6 +222,7 @@ export class ProjectEditAddComponent implements OnInit, OnDestroy {
   isSripeSub: boolean;
   salesEmail: string;
   public hideHelpLink: boolean;
+  public hideExtremeMeasures: boolean;
 
   formErrors: FormErrors = {
     'creditCard': '',
@@ -283,6 +284,8 @@ export class ProjectEditAddComponent implements OnInit, OnDestroy {
     if (brand) {
       this.contactUsEmail = brand['CONTACT_US_EMAIL'];
       this.salesEmail = brand['CONTACT_SALES_EMAIL'];
+      this.hideExtremeMeasures = brand['EXTREME_MEASURES']
+      // console.log('[PRJCT-EDIT-ADD] hideExtremeMeasures ',  this.hideExtremeMeasures)
     }
     // this.translationParams = { plan_name: PLAN_NAME.B } // Scale
     this.translationParams = { plan_name: PLAN_NAME.E } // Premium
