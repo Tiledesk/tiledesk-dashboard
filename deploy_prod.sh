3be7ab4e7d9ff9ead6aae8e996b2b003745a9f3e
Nicola Lanzilotto <nicola@tiledesk.com>
2024-11-04 17:16:33 +0100

    minor improvements

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -12,15 +12,15 @@
 node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng build --configuration production --output-path=dist --base-href ./
 cd dist
 
-# aws s3 sync --cache-control max-age=172800 . s3://panel.tiledesk.com/v3/dashboard/
-# aws cloudfront create-invalidation --distribution-id E2D4FS8NGUODM4 --paths "/*"
-# echo new version deployed on s3://panel.tiledesk.com/v3/dashboard/
-# echo available on https://panel.tiledesk.com/v3/dashboard/index.html
-
-aws s3 sync --cache-control max-age=172800 . s3://panel.tiledesk.com/v3/dashboard/ps
+aws s3 sync --cache-control max-age=172800 . s3://panel.tiledesk.com/v3/dashboard/
 aws cloudfront create-invalidation --distribution-id E2D4FS8NGUODM4 --paths "/*"
-echo new version deployed on s3://panel.tiledesk.com/v3/dashboard/ps
-echo available on https://panel.tiledesk.com/v3/dashboard/ps/index.html
+echo new version deployed on s3://panel.tiledesk.com/v3/dashboard/
+echo available on https://panel.tiledesk.com/v3/dashboard/index.html
+
+# aws s3 sync --cache-control max-age=172800 . s3://panel.tiledesk.com/v3/dashboard/ps
+# aws cloudfront create-invalidation --distribution-id E2D4FS8NGUODM4 --paths "/*"
+# echo new version deployed on s3://panel.tiledesk.com/v3/dashboard/ps
+# echo available on https://panel.tiledesk.com/v3/dashboard/ps/index.html
 
 
 
