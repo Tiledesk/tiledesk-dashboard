c82b710636cd0baad5034d85e7299be0abd12908
Nicola Lanzilotto <nicola@tiledesk.com>
2024-11-19 18:05:26 +0100

    Adds the ability to add the webhook subscriptions "Request Abandoned" and "Request Fully Abandoned" (improvements)

--- src/app/webhook/webhook-add-edit/webhook-add-edit.component.ts
+++ src/app/webhook/webhook-add-edit/webhook-add-edit.component.ts
@@ -59,8 +59,8 @@ export class WebhookAddEditComponent implements OnInit {
     { id: "group.delete", name:"Group Delete"},
     { id: "operator.select", name:"Operator Selection"},
     { id: "event.emit", name:"Event Emit"},
-    { id: "smart_assignment.request_abandoned", name:"Request Abandoned"},
-    { id: "request.fully_abandoned", name:"Request Fully Abandoned"},
+    { id: "event.emit.smart_assignment.request_abandoned", name:"Request Abandoned"},
+    { id: "event.emit.request.fully_abandoned", name:"Request Fully Abandoned"},
   ]
 
   constructor(
