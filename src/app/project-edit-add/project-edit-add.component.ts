6b541ca70cd263f2a6c23aaa064c8b6204b10a78
Nicola Lanzilotto <nicola@tiledesk.com>
2024-11-25 16:07:35 +0100

    Dashboard UI refactoring

--- src/app/project-edit-add/project-edit-add.component.ts
+++ src/app/project-edit-add/project-edit-add.component.ts
@@ -2204,7 +2204,7 @@ export class ProjectEditAddComponent implements OnInit, OnDestroy {
         this.CARD_HAS_ERROR = true;
         this.SPINNER_IN_ADD_CARD_MODAL = false
         this.DISPLAY_ADD_CARD_COMPLETED = false
-      } else if (error_body && !error_body.msg) {
+      } else if (error_body && (!error_body.msg || error_body.msg === undefined)) {
         if (error_body && error_body.error && error_body.error && error_body.error.msg.code) {
           this.credit_card_error_msg = error_body.error.msg.code;
           this.CARD_HAS_ERROR = true;
