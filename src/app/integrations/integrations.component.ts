c89bcf4b1b6320983b00a276ecf206fd942ddcb6
Nicola Lanzilotto <nicola@tiledesk.com>
2025-03-25 10:42:14 +0100

    Improves the integration Ollama

--- src/app/integrations/integrations.component.ts
+++ src/app/integrations/integrations.component.ts
@@ -1,7 +1,7 @@
 import { Component, isDevMode, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'app/core/auth.service';
 import { IntegrationService } from 'app/services/integration.service';
-import { APPS_TITLE, BrevoIntegration, N8nIntegration, CATEGORIES_LIST, CustomerioIntegration, HubspotIntegration, INTEGRATIONS_CATEGORIES, INTEGRATIONS_KEYS, INTEGRATION_LIST_ARRAY, MakeIntegration, OpenaiIntegration, QaplaIntegration, INTEGRATION_LIST_ARRAY_CLONE, GoogleIntegration, AnthropicIntegration, GroqIntegration, CohereIntegration, DeepseekIntegration} from './utils'; // OllamaIntegration, DeepseekIntegration
+import { APPS_TITLE, BrevoIntegration, N8nIntegration, CATEGORIES_LIST, CustomerioIntegration, HubspotIntegration, INTEGRATIONS_CATEGORIES, INTEGRATIONS_KEYS, INTEGRATION_LIST_ARRAY, MakeIntegration, OpenaiIntegration, QaplaIntegration, INTEGRATION_LIST_ARRAY_CLONE, GoogleIntegration, AnthropicIntegration, GroqIntegration, CohereIntegration, DeepseekIntegration, OllamaIntegration} from './utils'; // , DeepseekIntegration
 import { LoggerService } from 'app/services/logger/logger.service';
 import { NotifyService } from 'app/core/notify.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -584,9 +584,9 @@ export class IntegrationsComponent implements OnInit, OnDestroy {
       return new CohereIntegration();
     }
 
-    // if (key === INTEGRATIONS_KEYS.OLLAMA) {
-    //   return new OllamaIntegration();
-    // }
+    if (key === INTEGRATIONS_KEYS.OLLAMA) {
+      return new OllamaIntegration();
+    }
 
     if (key === INTEGRATIONS_KEYS.DEEPSEEK) {
       return new DeepseekIntegration();
