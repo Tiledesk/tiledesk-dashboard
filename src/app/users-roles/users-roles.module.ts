6ec89fa206f2964241fac946284efec1decb993d
Nicola Lanzilotto <nicola@tiledesk.com>
2025-05-27 14:59:04 +0200

    Adds a tooltip on the "Edit" and "Delete" buttons for the predefined roles

--- src/app/users-roles/users-roles.module.ts
+++ src/app/users-roles/users-roles.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { SettingsSidebarModule } from 'app/components/settings-sidebar/settings-sidebar.module';
 import { SharedModule } from 'app/shared/shared.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const routes: Routes = [
   { path: "", component: UsersRolesComponent },
@@ -20,6 +21,7 @@ const routes: Routes = [
     TranslateModule,
     SettingsSidebarModule,
     SharedModule,
+    MatTooltipModule
   ]
 })
 export class UsersRolesModule { }
