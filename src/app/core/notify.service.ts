f8e9784b0b486feadcdd963171d4bb1b8f749012
Nicola Lanzilotto <nicola@tiledesk.com>
2025-07-21 15:26:29 +0200

    Adds traslations

--- src/app/core/notify.service.ts
+++ src/app/core/notify.service.ts
@@ -175,9 +175,9 @@ export class NotifyService {
 
     Swal.fire({
       icon: 'warning',
-      title: 'Permission Denied',
-      text: 'We are sorry, but you don\'t have permissions to view this section.',
-      confirmButtonText: 'OK',
+      title: this.translate.instant('PermissionDenied'),
+      text: this.translate.instant('YonDontHavePermissionsToPerformThisAction'),
+      confirmButtonText: this.translate.instant('Ok'),
       confirmButtonColor: "var(--blue-light)",
       focusConfirm: true,
       customClass: CHAT_PANEL_MODE === true ? "swal-size-sm" : "",
