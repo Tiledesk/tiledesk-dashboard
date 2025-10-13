4e1eb1ca6010dc54c3dcf876c132d09b4ad50183
Nicola Lanzilotto <nicola@tiledesk.com>
2025-10-13 12:16:10 +0200

    Removes Create ticket permission

--- src/app/users-new-role/users-new-role.component.ts
+++ src/app/users-new-role/users-new-role.component.ts
@@ -120,7 +120,7 @@ export class UsersNewRoleComponent implements OnInit {
         { key: PERMISSIONS.INBOX_READ, label: 'Able to view real time and opened' },
         { key: PERMISSIONS.HISTORY_READ, label: 'Able to view History' },
         // { key: PERMISSIONS.REQUEST_UPDATE, label: 'Able to update' },
-        { key: PERMISSIONS.REQUEST_CREATE_TICKET, label: 'Able to create a ticket'},
+        // { key: PERMISSIONS.REQUEST_CREATE_TICKET, label: 'Able to create a ticket'},
         { key: PERMISSIONS.REQUEST_SEND, label: 'Able to send message and upload file' },
         { key: PERMISSIONS.REQUEST_JOIN, label: 'Able to join to a conversation' },
         { key: PERMISSIONS.REQUEST_CLOSE, label: 'Able to resolve a conversation' },
