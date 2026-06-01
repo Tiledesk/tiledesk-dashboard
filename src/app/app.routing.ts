07ef24b75bb33d2f94368ce5c1f7eb197af7e218
Nicola Lanzilotto <nicola@tiledesk.com>
2026-06-01 12:38:07 +0200

    Adds data table routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1259,6 +1259,15 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
 
+  // ---------------------------
+  // Data tables
+  // ---------------------------
+  {
+    path: 'project/:projectid/data-tables',
+    loadChildren: () => import('app/data-tables/data-tables.module').then(m => m.DataTablesModule),
+    canActivate: [AuthGuard]
+  },
+
   // Check moved in RoleService
   // {
   //   path: 'project/:projectid/knowledge-bases/:namespaceid',
