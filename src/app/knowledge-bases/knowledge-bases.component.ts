15e52080aa1b244865ce5e6158f7a87cca525c66
Nicola <lanzilottonicola74@gmail.com>
2026-06-09 16:34:43 +0200

    Fixes the bug: Knowledge base statistics links to new analytics don't work

--- src/app/knowledge-bases/knowledge-bases.component.ts
+++ src/app/knowledge-bases/knowledge-bases.component.ts
@@ -4793,7 +4793,7 @@ _presentDialogImportContents() {
       from: startDate,
       to: endDate,
     });
-    this.router.navigate(['project', this.id_project, 'analytics', 'new']);
+    this.router.navigate(['project', this.id_project, 'analytics']);
   }
 
   private renderKbCharts(): void {
